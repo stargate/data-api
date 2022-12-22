33817ca6cd724c794ce26a74e387f881440e2421
Tatu Saloranta <87213665+tatu-at-datastax@users.noreply.github.com>
2022-12-22 14:12:13 -0800

    Initial incomplete implementation of shredding (doc->shredded) (#12)

--- src/main/java/io/stargate/sgv3/docsapi/exception/ErrorCode.java
+++ src/main/java/io/stargate/sgv3/docsapi/exception/ErrorCode.java
@@ -4,7 +4,17 @@ package io.stargate.sgv3.docsapi.exception;
 public enum ErrorCode {
 
   /** Command error codes. */
-  COMMAND_NOT_IMPLEMENTED("The provided command is not implemented.");
+  COMMAND_NOT_IMPLEMENTED("The provided command is not implemented."),
+
+  SHRED_BAD_DOCUMENT_TYPE("Bad document type to shred"),
+
+  SHRED_BAD_DOCID_TYPE("Bad type for '_id' property"),
+
+  SHRED_INTERNAL_NO_PATH("Internal: path being built does not point to a property or element"),
+
+  SHRED_NO_MD5("MD5 Hash algorithm not available"),
+
+  SHRED_UNRECOGNIZED_NODE_TYPE("Unrecognized JSON node type in input document");
 
   private final String message;
 
--- src/main/java/io/stargate/sgv3/docsapi/service/shredding/Shredder.java
+++ src/main/java/io/stargate/sgv3/docsapi/service/shredding/Shredder.java
@@ -1,9 +1,17 @@
 package io.stargate.sgv3.docsapi.service.shredding;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.stargate.sgv3.docsapi.exception.DocsException;
+import io.stargate.sgv3.docsapi.exception.ErrorCode;
+import io.stargate.sgv3.docsapi.service.shredding.model.DocValueHasher;
 import io.stargate.sgv3.docsapi.service.shredding.model.WritableShreddedDocument;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
-import javax.validation.constraints.NotNull;
 
 /**
  * Shred an incoming JSON document into the data we need to store in the DB, and then de-shred.
@@ -21,8 +29,105 @@ public class Shredder {
    * @param document {@link JsonNode} to shred.
    * @return WritableShreddedDocument
    */
-  public WritableShreddedDocument shred(@NotNull JsonNode document) {
-    // TODO @tatu implement me
-    return new WritableShreddedDocument();
+  public WritableShreddedDocument shred(JsonNode document) {
+    return shred(document, Optional.empty());
+  }
+
+  public WritableShreddedDocument shred(JsonNode doc, Optional<UUID> txId) {
+    // 13-Dec-2022, tatu: Although we could otherwise allow non-Object documents, requirement
+    //    to have the _id (or at least place for it) means we cannot allow that.
+    if (!doc.isObject()) {
+      throw new DocsException(
+          ErrorCode.SHRED_BAD_DOCUMENT_TYPE,
+          String.format(
+              "%s: Document to shred must be a JSON Object, instead got %s",
+              ErrorCode.SHRED_BAD_DOCUMENT_TYPE.getMessage(), doc.getNodeType()));
+    }
+
+    ObjectNode docOb = (ObjectNode) doc;
+
+    // We will extract id if there is one; stored separately, removed so we won't process
+    // it, add path or such (since it has specific meaning separate from general properties)
+    JsonNode idNode = docOb.remove("_id");
+    String id = null;
+
+    // Cannot require existence as id often auto-generated when inserting: caller has to verify
+    // if existence required
+    if (idNode != null) {
+      if (!idNode.isTextual()) {
+        throw new DocsException(
+            ErrorCode.SHRED_BAD_DOCID_TYPE,
+            String.format(
+                "%s: Document Id must be a JSON String, instead got %s",
+                ErrorCode.SHRED_BAD_DOCID_TYPE.getMessage(), idNode.getNodeType()));
+      }
+      id = idNode.asText();
+    }
+
+    final WritableShreddedDocument.Builder b =
+        WritableShreddedDocument.builder(new DocValueHasher(), id, txId);
+    traverse(doc, b, JsonPath.rootBuilder());
+    return b.build();
+  }
+
+  /**
+   * Main traversal method we need to produce callbacks to passed-in listener; used to separate
+   * shredding logic from that of recursive-descent traversal.
+   */
+  private void traverse(JsonNode doc, ShredListener callback, JsonPath.Builder pathBuilder) {
+    // NOTE: main level is handled bit differently; no callbacks for Objects or Arrays,
+    // only for the (rare) case of atomic values. Just traversal.
+
+    if (doc.isObject()) {
+      traverseObject((ObjectNode) doc, callback, pathBuilder);
+    } else if (doc.isArray()) {
+      traverseArray((ArrayNode) doc, callback, pathBuilder);
+    } else {
+      traverseValue(doc, callback, pathBuilder);
+    }
+  }
+
+  private void traverseObject(
+      ObjectNode obj, ShredListener callback, JsonPath.Builder pathBuilder) {
+
+    Iterator<Map.Entry<String, JsonNode>> it = obj.fields();
+    while (it.hasNext()) {
+      Map.Entry<String, JsonNode> entry = it.next();
+      pathBuilder.property(entry.getKey());
+      traverseValue(entry.getValue(), callback, pathBuilder);
+    }
+  }
+
+  private void traverseArray(ArrayNode arr, ShredListener callback, JsonPath.Builder pathBuilder) {
+    int ix = 0;
+    for (JsonNode value : arr) {
+      pathBuilder.index(ix++);
+      traverseValue(value, callback, pathBuilder);
+    }
+  }
+
+  private void traverseValue(JsonNode value, ShredListener callback, JsonPath.Builder pathBuilder) {
+    if (value.isObject()) {
+      ObjectNode ob = (ObjectNode) value;
+      callback.shredObject(pathBuilder, ob);
+      traverseObject(ob, callback, pathBuilder.nestedValueBuilder());
+    } else if (value.isArray()) {
+      ArrayNode arr = (ArrayNode) value;
+      callback.shredArray(pathBuilder, arr);
+      traverseArray(arr, callback, pathBuilder.nestedValueBuilder());
+    } else if (value.isTextual()) {
+      callback.shredText(pathBuilder.build(), value.textValue());
+    } else if (value.isNumber()) {
+      callback.shredNumber(pathBuilder.build(), value.decimalValue());
+    } else if (value.isBoolean()) {
+      callback.shredBoolean(pathBuilder.build(), value.booleanValue());
+    } else if (value.isNull()) {
+      callback.shredNull(pathBuilder.build());
+    } else {
+      throw new DocsException(
+          ErrorCode.SHRED_UNRECOGNIZED_NODE_TYPE,
+          String.format(
+              "%s: %s", ErrorCode.SHRED_UNRECOGNIZED_NODE_TYPE.getMessage(), value.getNodeType()));
+    }
   }
 }
