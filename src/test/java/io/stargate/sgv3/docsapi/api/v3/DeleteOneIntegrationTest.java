8d696639086ddb7392090d84bcd9196d82f8c686
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-01-18 12:42:09 -0500

    Removed the CollectionResourceIntegrationTestBase.java and made the tests ordered

--- src/test/java/io/stargate/sgv3/docsapi/api/v3/DeleteOneIntegrationTest.java
+++ src/test/java/io/stargate/sgv3/docsapi/api/v3/DeleteOneIntegrationTest.java
@@ -6,9 +6,13 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.stargate.sgv2.api.common.config.constants.HttpConstants;
+import io.stargate.sgv2.common.CqlEnabledIntegrationTestBase;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.hamcrest.collection.IsCollectionWithSize;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
@@ -19,7 +23,38 @@ import org.junit.jupiter.api.TestMethodOrder;
 @QuarkusIntegrationTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class DeleteOneIntegrationTest extends CollectionResourceIntegrationTestBase {
+public class DeleteOneIntegrationTest extends CqlEnabledIntegrationTestBase {
+
+  protected String collectionName = "col" + RandomStringUtils.randomNumeric(16);
+
+  @BeforeAll
+  public static void enableLog() {
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+  }
+
+  @Test
+  @Order(1)
+  public final void createCollection() {
+    String json =
+        String.format(
+            """
+                                {
+                                  "createCollection": {
+                                    "name": "%s"
+                                  }
+                                }
+                                """,
+            collectionName);
+    given()
+        .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, getAuthToken())
+        .contentType(ContentType.JSON)
+        .body(json)
+        .when()
+        .post(DatabaseResource.BASE_PATH, keyspaceId.asInternal())
+        .then()
+        .statusCode(200);
+  }
+
   @Nested
   class DeleteOne {
     @Test
