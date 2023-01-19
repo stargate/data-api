276370eae2c48c0b45234a80e4fe52ed45512159
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-01-19 06:54:00 -0500

    Changed example for FilterClause

--- src/main/java/io/stargate/sgv3/docsapi/api/model/command/clause/filter/FilterClause.java
+++ src/main/java/io/stargate/sgv3/docsapi/api/model/command/clause/filter/FilterClause.java
@@ -11,6 +11,6 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
     type = SchemaType.OBJECT,
     implementation = Object.class,
     example = """
-              {"col1": "val1", "col2": "val"}
+             {"name": "Aaron", "country": "US"}
               """)
 public record FilterClause(List<ComparisonExpression> comparisonExpressions) {}
