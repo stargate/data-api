963570bf0a9c64a0f24de7c95b3b3ec6f693b5ae
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-01-26 21:20:44 -0800

    Javadoc improvement

--- src/main/java/io/stargate/sgv3/docsapi/api/model/command/clause/update/UpdateOperation.java
+++ src/main/java/io/stargate/sgv3/docsapi/api/model/command/clause/update/UpdateOperation.java
@@ -5,10 +5,18 @@ import io.stargate.sgv3.docsapi.config.constants.DocumentConstants;
 import io.stargate.sgv3.docsapi.exception.DocsException;
 import io.stargate.sgv3.docsapi.exception.ErrorCode;
 
-/** UpdateOperation represents definition of one of update definitions from {@link UpdateClause} */
+/**
+ * UpdateOperation represents one of update definitions from {@link UpdateClause} (like {@code $set}
+ * or {@code $unset}): single operation can contain multiple actual changes.
+ */
 public abstract class UpdateOperation {
   public abstract void updateDocument(ObjectNode doc);
 
+  /**
+   * Shared validation method used by {@code $set} and {@code $unset} operations to ensure they are
+   * not used to modify paths that are not allowed (specifically Document's primary id, {@code
+   * _id}).
+   */
   protected static String validateSetPath(UpdateOperator oper, String path) {
     if (DocumentConstants.Fields.DOC_ID.equals(path)) {
       throw new DocsException(
