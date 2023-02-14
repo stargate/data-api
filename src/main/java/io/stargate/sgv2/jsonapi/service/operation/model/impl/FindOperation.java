39f794838e04bb021c42310160fd60e72acd5209
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-02-14 16:20:54 -0500

    Added comments for the ReadType

--- src/main/java/io/stargate/sgv2/jsonapi/service/operation/model/impl/FindOperation.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/operation/model/impl/FindOperation.java
@@ -84,9 +84,18 @@ public record FindOperation(
     return null;
   }
 
+  /** Read type specifies what data needs to be returned as part of the response */
   public enum ReadType {
+    /**
+     * Return documents and transaction id which satisfies the filter conditions as part of response
+     */
     DOCUMENT,
+    /**
+     * Return only document id and transaction id of documents which satisfies the filter conditions
+     * as part of response
+     */
     KEY,
+    /** Return only count of documents which satisfies the filter condition */
     COUNT
   }
 }
