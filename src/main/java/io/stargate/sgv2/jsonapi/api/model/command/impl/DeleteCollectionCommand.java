22e5de7c89a8c5b1e5843f3c246cda06db9f0ff6
Ivan Senic <ivan.senic@datastax.com>
2023-02-20 16:48:45 +0100

    fix schema in the collection commands (#157)

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/impl/DeleteCollectionCommand.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/impl/DeleteCollectionCommand.java
@@ -3,7 +3,6 @@ package io.stargate.sgv2.jsonapi.api.model.command.impl;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.stargate.sgv2.jsonapi.api.model.command.NamespaceCommand;
 import javax.validation.constraints.NotBlank;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 /**
@@ -14,10 +13,5 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 @Schema(description = "Command that deletes a collection if one exists.")
 @JsonTypeName("deleteCollection")
 public record DeleteCollectionCommand(
-    @NotBlank
-        @Schema(
-            description = "Name of the collection",
-            implementation = Object.class,
-            type = SchemaType.OBJECT)
-        String name)
+    @NotBlank @Schema(description = "Name of the collection") String name)
     implements NamespaceCommand {}
