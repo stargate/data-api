cd8c65a5cde49c194dfa03d33d62f112b602618a
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-03-08 11:41:19 -0800

    Add skeletal $min/$max handlers

---
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/clause/update/MinMaxOperation.java
@@ -0,0 +1,78 @@
+package io.stargate.sgv2.jsonapi.api.model.command.clause.update;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementation of {@code $inc} update operation used to modify numeric field values in documents.
+ * See {@href https://www.mongodb.com/docs/manual/reference/operator/update/inc/} for full
+ * explanation.
+ */
+public class MinMaxOperation extends UpdateOperation {
+  private final List<MinMaxAction> actions;
+
+  private final boolean isMaxAction;
+
+  private MinMaxOperation(boolean isMaxAction, List<MinMaxAction> actions) {
+    this.isMaxAction = isMaxAction;
+    this.actions = sortByPath(actions);
+  }
+
+  public static MinMaxOperation constructMax(ObjectNode args) {
+    return construct(args, UpdateOperator.MAX, true);
+  }
+
+  public static MinMaxOperation constructMin(ObjectNode args) {
+    return construct(args, UpdateOperator.MIN, false);
+  }
+
+  private static MinMaxOperation construct(ObjectNode args, UpdateOperator oper, boolean isMax) {
+    Iterator<Map.Entry<String, JsonNode>> fieldIter = args.fields();
+
+    List<MinMaxAction> actions = new ArrayList<>();
+    while (fieldIter.hasNext()) {
+      Map.Entry<String, JsonNode> entry = fieldIter.next();
+      // Verify we do not try to change doc id
+      String path = validateUpdatePath(oper, entry.getKey());
+      actions.add(new MinMaxAction(path, entry.getValue()));
+    }
+    return new MinMaxOperation(isMax, actions);
+  }
+
+  @Override
+  public boolean updateDocument(ObjectNode doc, UpdateTargetLocator targetLocator) {
+    // Almost always changes, except if adding zero; need to track
+    boolean modified = false;
+    for (MinMaxAction action : actions) {
+      final String path = action.path;
+      final JsonNode value = action.value;
+
+      UpdateTarget target = targetLocator.findOrCreate(doc, path);
+      JsonNode oldValue = target.valueNode();
+
+      if (oldValue == null) { // No such property? Add value
+        target.replaceValue(value);
+        modified = true;
+      } else { // Otherwise, need to see if less-than (min) or greater-than (max)
+        if (shouldReplace(oldValue, value)) {
+          target.replaceValue(value);
+          modified = true;
+        }
+      }
+    }
+
+    return modified;
+  }
+
+  private boolean shouldReplace(JsonNode oldValue, JsonNode newValue) {
+    // !!! TODO
+    return true;
+  }
+
+  /** Value class for per-field update operations. */
+  private record MinMaxAction(String path, JsonNode value) implements ActionWithPath {}
+}
