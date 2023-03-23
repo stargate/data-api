1a8244fb24c7116cd8d964dba6e2c95c2253a40f
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-03-23 14:58:04 -0700

    Start actual work on projection def reading

--- src/main/java/io/stargate/sgv2/jsonapi/service/projection/DocumentProjector.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/projection/DocumentProjector.java
@@ -1,8 +1,12 @@
 package io.stargate.sgv2.jsonapi.service.projection;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import io.stargate.sgv2.jsonapi.config.constants.DocumentConstants;
 import io.stargate.sgv2.jsonapi.exception.ErrorCode;
 import io.stargate.sgv2.jsonapi.exception.JsonApiException;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Helper class that implements functionality needed to support projections on documents fetched via
@@ -28,6 +32,8 @@ public class DocumentProjector {
     if (projectionDefinition.isEmpty()) {
       return identityProjector();
     }
+    PathCollector paths = PathCollector.collectPaths(projectionDefinition);
+
     return new DocumentProjector();
   }
 
@@ -51,5 +57,98 @@ public class DocumentProjector {
   @Override
   public int hashCode() {
     return 1;
+  }
+
+  /** Helper object used to traverse and collection inclusion/exclusion definitions */
+  private static class PathCollector {
+    private Set<String> exclusions;
+    private Set<String> inclusions;
+
+    private Boolean idInclusion = null;
+
+    private PathCollector() {}
+
+    static PathCollector collectPaths(JsonNode def) {
+      return new PathCollector().collectFromObject(def, null);
+    }
+
+    PathCollector collectFromObject(JsonNode ob, String parentPath) {
+      var it = ob.fields();
+      while (it.hasNext()) {
+        var entry = it.next();
+        String path = entry.getKey();
+        if (parentPath != null) {
+          path = parentPath + "." + path;
+        }
+        JsonNode value = entry.getValue();
+        if (value.isNumber()) {
+          // "0" means exclude (like false); any other number include
+          if (BigDecimal.ZERO.equals(value.decimalValue())) {
+            addExclusion(path);
+          } else {
+            addInclusion(path);
+          }
+        } else if (value.isBoolean()) {
+          if (value.booleanValue()) {
+            addInclusion(path);
+          } else {
+            addExclusion(path);
+          }
+        } else if (value.isObject()) {
+          // Nested definitions allowed, too
+          collectFromObject(value, path);
+        } else {
+          // Unknown JSON node type; error
+          throw new JsonApiException(
+              ErrorCode.UNSUPPORTED_PROJECTION_PARAM,
+              ErrorCode.UNSUPPORTED_PROJECTION_PARAM.getMessage()
+                  + ": path ('"
+                  + path
+                  + "') value must be NUMBER, BOOLEAN or OBJECT, was "
+                  + value.getNodeType());
+        }
+      }
+      return this;
+    }
+
+    private void addExclusion(String path) {
+      if (DocumentConstants.Fields.DOC_ID.equals(path)) {
+        idInclusion = false;
+      } else {
+        if (exclusions == null) {
+          // Must not mix exclusions and inclusions
+          if (inclusions != null) {
+            throw new JsonApiException(
+                ErrorCode.UNSUPPORTED_PROJECTION_PARAM,
+                ErrorCode.UNSUPPORTED_PROJECTION_PARAM.getMessage()
+                    + ": cannot exclude '"
+                    + path
+                    + "' on inclusion projection");
+          }
+          exclusions = new HashSet<>();
+        }
+        exclusions.add(path);
+      }
+    }
+
+    private void addInclusion(String path) {
+      if (DocumentConstants.Fields.DOC_ID.equals(path)) {
+        idInclusion = true;
+      } else {
+        if (inclusions == null) {
+          // Must not mix exclusions and inclusions
+          if (exclusions != null) {
+            throw new JsonApiException(
+                ErrorCode.UNSUPPORTED_PROJECTION_PARAM,
+                ErrorCode.UNSUPPORTED_PROJECTION_PARAM.getMessage()
+                    + ": cannot include '"
+                    + path
+                    + "' on exclusion projection");
+          }
+          inclusions = new HashSet<>();
+        }
+        inclusions.add(path);
+      }
+    }
   }
 }
--- src/test/java/io/stargate/sgv2/jsonapi/service/projection/DocumentProjectorTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/projection/DocumentProjectorTest.java
@@ -33,9 +33,50 @@ public class DocumentProjectorTest {
     }
 
     @Test
+    public void verifyNoIncludeAfterExclude() throws Exception {
+      JsonNode def =
+          objectMapper.readTree(
+              """
+              { "excludeMe" : 0,
+                "excludeMeToo" : 0,
+                "include.me" : 1
+              }
+              """);
+      Throwable t = catchThrowable(() -> DocumentProjector.createFromDefinition(def));
+      assertThat(t)
+          .isInstanceOf(JsonApiException.class)
+          .hasFieldOrPropertyWithValue("errorCode", ErrorCode.UNSUPPORTED_PROJECTION_PARAM)
+          .hasMessage(
+              "Unsupported projection parameter: cannot include 'include.me' on exclusion projection");
+    }
+
+    @Test
+    public void verifyNoExcludeAfterInclude() throws Exception {
+      JsonNode def =
+          objectMapper.readTree(
+              """
+              { "includeMe" : 1,
+                "misc" : {
+                   "nested": {
+                     "do" : true,
+                     "dont" : false
+                    }
+                },
+                "includeMe2" : 1
+              }
+              """);
+      Throwable t = catchThrowable(() -> DocumentProjector.createFromDefinition(def));
+      assertThat(t)
+          .isInstanceOf(JsonApiException.class)
+          .hasFieldOrPropertyWithValue("errorCode", ErrorCode.UNSUPPORTED_PROJECTION_PARAM)
+          .hasMessage(
+              "Unsupported projection parameter: cannot exclude 'misc.nested.dont' on inclusion projection");
+    }
+
+    @Test
     public void verifyProjectionEquality() throws Exception {
-      String defStr1 = "{ \"field1\" : 1, \"field2\": 0 }";
-      String defStr2 = "{ \"field1\" : 0, \"field2\": 1 }";
+      String defStr1 = "{ \"field1\" : 1, \"field2\": 1 }";
+      String defStr2 = "{ \"field1\" : 1, \"field3\": 1 }";
 
       DocumentProjector proj1 =
           DocumentProjector.createFromDefinition(objectMapper.readTree(defStr1));
