966be59d823286853ee89586a97c88632f3f8759
Ivan Senic <ivan.senic@datastax.com>
2023-03-29 14:34:35 +0200

    closes #261: grpc retries and timeouts to fix availability failures

---
+++ src/main/java/io/stargate/sgv2/jsonapi/grpc/retries/impl/JsonApiGrpcRetryPolicy.java
@@ -0,0 +1,54 @@
+package io.stargate.sgv2.jsonapi.grpc.retries.impl;
+
+import io.grpc.Metadata;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.protobuf.ProtoUtils;
+import io.stargate.bridge.proto.QueryOuterClass;
+import java.util.Objects;
+import java.util.function.Predicate;
+import javax.enterprise.context.ApplicationScoped;
+
+/** Default gRPC retry policy used in the project. */
+@ApplicationScoped
+// TODO correct type here
+public class JsonApiGrpcRetryPolicy implements Predicate<StatusRuntimeException> {
+
+  private static final Metadata.Key<QueryOuterClass.WriteTimeout> WRITE_TIMEOUT_KEY =
+      ProtoUtils.keyForProto(QueryOuterClass.WriteTimeout.getDefaultInstance());
+
+  private static final Metadata.Key<QueryOuterClass.ReadTimeout> READ_TIMEOUT_KEY =
+      ProtoUtils.keyForProto(QueryOuterClass.ReadTimeout.getDefaultInstance());
+
+  /** {@inheritDoc} */
+  @Override
+  public boolean test(StatusRuntimeException e) {
+    Status status = e.getStatus();
+    Status.Code code = status.getCode();
+
+    // always retry unavailable
+    if (Objects.equals(code, Status.Code.UNAVAILABLE)) {
+      return true;
+    }
+
+    // for timeouts, retry only server side timeouts
+    if (Objects.equals(code, Status.Code.DEADLINE_EXCEEDED)) {
+      return isValidServerSideTimeout(e.getTrailers());
+    }
+
+    // nothing else
+    return false;
+  }
+
+  // ensure we retry only server side timeouts we want
+  private boolean isValidServerSideTimeout(Metadata trailers) {
+    // if we have trailers
+    if (null != trailers) {
+      // TODO double check the CAS write timeout retries are fine
+      return trailers.containsKey(READ_TIMEOUT_KEY) || trailers.containsKey(WRITE_TIMEOUT_KEY);
+    }
+
+    // otherwise not
+    return false;
+  }
+}
