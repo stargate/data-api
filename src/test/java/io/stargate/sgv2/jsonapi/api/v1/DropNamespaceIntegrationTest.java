1caac2fa9af78c137686b01711583f76eb8c53c9
Ivan Senic <ivan.senic@datastax.com>
2023-04-04 16:10:55 +0200

    with existing collection

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/DropNamespaceIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/DropNamespaceIntegrationTest.java
@@ -15,6 +15,7 @@ import io.restassured.http.ContentType;
 import io.stargate.sgv2.api.common.config.constants.HttpConstants;
 import io.stargate.sgv2.common.CqlEnabledIntegrationTestBase;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -61,6 +62,79 @@ class DropNamespaceIntegrationTest extends CqlEnabledIntegrationTestBase {
               row -> {
                 assertThat(row.get("keyspace_name", String.class))
                     .isNotEqualTo(keyspaceId.asInternal());
+              });
+    }
+
+    @Test
+    public final void withExistingCollection() {
+      String keyspace = "k%s".formatted(RandomStringUtils.randomAlphanumeric(8)).toLowerCase();
+      String collection = "c%s".formatted(RandomStringUtils.randomAlphanumeric(8)).toLowerCase();
+
+      String createNamespace =
+          """
+              {
+                "createNamespace": {
+                  "name": "%s"
+                }
+              }
+              """
+              .formatted(keyspace);
+      String createCollection =
+          """
+              {
+                "createCollection": {
+                  "name": "%s"
+                }
+              }
+              """
+              .formatted(collection);
+
+      given()
+          .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, getAuthToken())
+          .contentType(ContentType.JSON)
+          .body(createNamespace)
+          .when()
+          .post(GeneralResource.BASE_PATH)
+          .then()
+          .statusCode(200)
+          .body("status.ok", is(1));
+      given()
+          .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, getAuthToken())
+          .contentType(ContentType.JSON)
+          .body(createCollection)
+          .when()
+          .post(NamespaceResource.BASE_PATH, keyspace)
+          .then()
+          .statusCode(200)
+          .body("status.ok", is(1));
+
+      String json =
+          """
+          {
+            "dropNamespace": {
+              "name": "%s"
+            }
+          }
+          """
+              .formatted(keyspace);
+
+      given()
+          .header(HttpConstants.AUTHENTICATION_TOKEN_HEADER_NAME, getAuthToken())
+          .contentType(ContentType.JSON)
+          .body(json)
+          .when()
+          .post(GeneralResource.BASE_PATH)
+          .then()
+          .statusCode(200)
+          .body("status.ok", is(1));
+
+      // ensure it's dropped
+      // TODO go away from session usage once we have findNamespaces
+      ResultSet keyspaces = session.execute("SELECT keyspace_name FROM system_schema.keyspaces;");
+      assertThat(keyspaces.all())
+          .allSatisfy(
+              row -> {
+                assertThat(row.get("keyspace_name", String.class)).isNotEqualTo(keyspace);
               });
     }
 
