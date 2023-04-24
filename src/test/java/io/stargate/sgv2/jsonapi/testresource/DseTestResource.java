cf30233969ba51b465e5fdebbf831909a8d3e022
Jeffrey Carpenter <jeffrey.carpenter@datastax.com>
2023-04-24 12:35:26 -0700

    update to dse 6.8.34 (#351)

--- src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
+++ src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
@@ -10,7 +10,7 @@ public class DseTestResource extends StargateTestResource {
     super();
 
     if (null == System.getProperty("testing.containers.cassandra-image")) {
-      System.setProperty("testing.containers.cassandra-image", "datastax/dse-server:6.8.33");
+      System.setProperty("testing.containers.cassandra-image", "datastax/dse-server:6.8.34");
     }
 
     if (null == System.getProperty("testing.containers.stargate-image")) {
