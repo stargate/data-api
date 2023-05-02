3d45b97d7e98d99433dbcd9222ba5c5050579e7d
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-05-02 13:22:07 -0700

    Fix #395: verify that "no options" Commands are not given any options

--- src/test/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/FindOneCommandResolverTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/FindOneCommandResolverTest.java
@@ -1,7 +1,9 @@
 package io.stargate.sgv2.jsonapi.service.resolver.model.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.Mock;
 import io.quarkus.test.junit.QuarkusTest;
@@ -9,6 +11,7 @@ import io.quarkus.test.junit.TestProfile;
 import io.stargate.sgv2.common.testprofiles.NoGlobalResourcesTestProfile;
 import io.stargate.sgv2.jsonapi.api.model.command.CommandContext;
 import io.stargate.sgv2.jsonapi.api.model.command.impl.FindOneCommand;
+import io.stargate.sgv2.jsonapi.exception.ErrorCode;
 import io.stargate.sgv2.jsonapi.service.operation.model.Operation;
 import io.stargate.sgv2.jsonapi.service.operation.model.ReadType;
 import io.stargate.sgv2.jsonapi.service.operation.model.impl.DBFilterBase;
@@ -34,12 +37,12 @@ public class FindOneCommandResolverTest {
     public void idFilterCondition() throws Exception {
       String json =
           """
-            {
-              "findOne": {
-                "filter" : {"_id" : "id"}
-              }
-            }
-            """;
+                          {
+                            "findOne": {
+                              "filter" : {"_id" : "id"}
+                            }
+                          }
+                          """;
 
       FindOneCommand command = objectMapper.readValue(json, FindOneCommand.class);
       Operation operation = resolver.resolveCommand(commandContext, command);
@@ -68,13 +71,13 @@ public class FindOneCommandResolverTest {
     public void filterConditionAndSort() throws Exception {
       String json =
           """
-            {
-              "findOne": {
-                "sort" : {"user.name" : 1, "user.age" : -1},
-                "filter" : {"status" : "active"}
-              }
-            }
-            """;
+                          {
+                            "findOne": {
+                              "sort" : {"user.name" : 1, "user.age" : -1},
+                              "filter" : {"status" : "active"}
+                            }
+                          }
+                          """;
 
       FindOneCommand command = objectMapper.readValue(json, FindOneCommand.class);
       Operation operation = resolver.resolveCommand(commandContext, command);
@@ -108,12 +111,41 @@ public class FindOneCommandResolverTest {
     public void noFilterCondition() throws Exception {
       String json =
           """
-            {
-              "findOne": {
+                          {
+                            "findOne": {
 
-              }
-            }
-            """;
+                            }
+                          }
+                          """;
+
+      FindOneCommand command = objectMapper.readValue(json, FindOneCommand.class);
+      Operation operation = resolver.resolveCommand(commandContext, command);
+
+      assertThat(operation)
+          .isInstanceOfSatisfying(
+              FindOperation.class,
+              op -> {
+                assertThat(op.objectMapper()).isEqualTo(objectMapper);
+                assertThat(op.commandContext()).isEqualTo(commandContext);
+                assertThat(op.limit()).isEqualTo(1);
+                assertThat(op.pageSize()).isEqualTo(1);
+                assertThat(op.pagingState()).isNull();
+                assertThat(op.readType()).isEqualTo(ReadType.DOCUMENT);
+                assertThat(op.filters()).isEmpty();
+                assertThat(op.singleResponse()).isTrue();
+              });
+    }
+
+    @Test
+    public void noFilterConditionEmptyOptions() throws Exception {
+      String json =
+          """
+                          {
+                            "findOne": {
+                                "options": { }
+                            }
+                          }
+                            """;
 
       FindOneCommand command = objectMapper.readValue(json, FindOneCommand.class);
       Operation operation = resolver.resolveCommand(commandContext, command);
@@ -137,12 +169,12 @@ public class FindOneCommandResolverTest {
     public void dynamicFilterCondition() throws Exception {
       String json =
           """
-            {
-              "findOne": {
-                "filter" : {"col" : "val"}
-              }
-            }
-            """;
+                          {
+                            "findOne": {
+                              "filter" : {"col" : "val"}
+                            }
+                          }
+                          """;
 
       FindOneCommand command = objectMapper.readValue(json, FindOneCommand.class);
       Operation operation = resolver.resolveCommand(commandContext, command);
@@ -164,6 +196,26 @@ public class FindOneCommandResolverTest {
                 assertThat(op.filters()).singleElement().isEqualTo(filter);
                 assertThat(op.singleResponse()).isTrue();
               });
+    }
+
+    // Only "empty" Options allowed, nothing else
+    @Test
+    public void illegalFilterConditionEmptyOptions() throws Exception {
+      String json =
+          """
+              {
+                "findOne": {
+                    "options": {
+                        "noSuchOption": "value"
+                    }
+                }
+              }
+                """;
+
+      Exception e = catchException(() -> objectMapper.readValue(json, FindOneCommand.class));
+      assertThat(e)
+          .isInstanceOf(JsonMappingException.class)
+          .hasMessage(ErrorCode.COMMAND_ACCEPTS_NO_OPTIONS.getMessage() + ": FindOneCommand");
     }
   }
 }
