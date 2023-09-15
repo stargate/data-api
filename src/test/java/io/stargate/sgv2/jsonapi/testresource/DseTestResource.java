aca8f8a91dcf266e5ce9f536ad35878ce17c85b3
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-09-15 12:55:35 -0400

    Override to pass the system properties for custom store

--- src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
+++ src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
@@ -1,6 +1,7 @@
 package io.stargate.sgv2.jsonapi.testresource;
 
 import io.stargate.sgv2.common.testresource.StargateTestResource;
+import java.util.Map;
 
 public class DseTestResource extends StargateTestResource {
 
@@ -26,5 +27,19 @@ public class DseTestResource extends StargateTestResource {
     if (null == System.getProperty("testing.containers.cluster-dse")) {
       System.setProperty("testing.containers.cluster-dse", "false");
     }
+  }
+
+  @Override
+  public Map<String, String> start() {
+    Map<String, String> env = super.start();
+    if (null != System.getProperty("stargate.jsonapi.embedding.service.custom.enabled")) {
+      env.put(
+          "stargate.jsonapi.embedding.service.custom.enabled",
+          System.getProperty("stargate.jsonapi.embedding.service.custom.enabled"));
+      env.put(
+          "stargate.jsonapi.embedding.service.custom.class-name",
+          System.getProperty("stargate.jsonapi.embedding.service.custom.class-name"));
+    }
+    return env;
   }
 }
