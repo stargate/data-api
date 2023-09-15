b7f8188de36317a2bd935b49d65a348a3b36b738
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-09-15 13:22:24 -0400

    Reverted the override change

--- src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
+++ src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
@@ -1,7 +1,6 @@
 package io.stargate.sgv2.jsonapi.testresource;
 
 import io.stargate.sgv2.common.testresource.StargateTestResource;
-import java.util.Map;
 
 public class DseTestResource extends StargateTestResource {
 
@@ -27,19 +26,5 @@ public class DseTestResource extends StargateTestResource {
     if (null == System.getProperty("testing.containers.cluster-dse")) {
       System.setProperty("testing.containers.cluster-dse", "false");
     }
-  }
-
-  @Override
-  public Map<String, String> start() {
-    Map<String, String> env = super.start();
-    if (null != System.getProperty("stargate.jsonapi.embedding.service.custom.enabled")) {
-      env.put(
-          "stargate.jsonapi.embedding.service.custom.enabled",
-          System.getProperty("stargate.jsonapi.embedding.service.custom.enabled"));
-      env.put(
-          "stargate.jsonapi.embedding.service.custom.class-name",
-          System.getProperty("stargate.jsonapi.embedding.service.custom.class-name"));
-    }
-    return env;
   }
 }
