432788cb712eabe79f1f193c30990f5f26dee75c
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-09-15 16:51:27 -0400

    Reverting the code change

--- src/main/java/io/stargate/sgv2/jsonapi/service/embedding/operation/OpenAiEmbeddingClient.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/embedding/operation/OpenAiEmbeddingClient.java
@@ -1,14 +1,18 @@
 package io.stargate.sgv2.jsonapi.service.embedding.operation;
 
+import dev.ai4j.openai4j.OpenAiClient;
+import dev.ai4j.openai4j.embedding.EmbeddingRequest;
+import dev.ai4j.openai4j.embedding.EmbeddingResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OpenAiEmbeddingClient implements EmbeddingService {
   private String modelName;
-  // OpenAiClient client;
+  OpenAiClient client;
 
   public OpenAiEmbeddingClient(String baseUrl, String apiKey, String modelName) {
     this.modelName = modelName;
-    // client = OpenAiClient.builder().openAiApiKey(apiKey).baseUrl(baseUrl).build();
+    client = OpenAiClient.builder().openAiApiKey(apiKey).baseUrl(baseUrl).build();
   }
 
   public OpenAiEmbeddingClient(String apiKey, String modelName) {
@@ -16,7 +20,7 @@ public class OpenAiEmbeddingClient implements EmbeddingService {
   }
 
   public List<float[]> vectorize(List<String> texts) {
-    /*EmbeddingRequest request = EmbeddingRequest.builder().input(texts).model(modelName).build();
+    EmbeddingRequest request = EmbeddingRequest.builder().input(texts).model(modelName).build();
     EmbeddingResponse response = execute(request);
     return response.data().stream()
         .map(openAiEmbedding -> openAiEmbedding.embedding())
@@ -28,11 +32,10 @@ public class OpenAiEmbeddingClient implements EmbeddingService {
               }
               return vector;
             })
-        .collect(Collectors.toList());*/
-    return null;
+        .collect(Collectors.toList());
   }
 
-  /*private EmbeddingResponse execute(EmbeddingRequest request) {
-    return null;//client.embedding(request).execute();
-  }*/
+  private EmbeddingResponse execute(EmbeddingRequest request) {
+    return client.embedding(request).execute();
+  }
 }
