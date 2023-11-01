b155aca19526ab7112c28922cbf86f4f3f84974e
Yuqi Du <istimdu@gmail.com>
2023-11-01 09:28:31 -0700

    error mapping

--- src/main/java/io/stargate/sgv2/jsonapi/exception/mappers/ThrowableToErrorMapper.java
+++ src/main/java/io/stargate/sgv2/jsonapi/exception/mappers/ThrowableToErrorMapper.java
@@ -1,7 +1,15 @@
 package io.stargate.sgv2.jsonapi.exception.mappers;
 
+import com.datastax.oss.driver.api.core.AllNodesFailedException;
+import com.datastax.oss.driver.api.core.DriverException;
+import com.datastax.oss.driver.api.core.DriverTimeoutException;
+import com.datastax.oss.driver.api.core.NoNodeAvailableException;
+import com.datastax.oss.driver.api.core.NodeUnavailableException;
+import com.datastax.oss.driver.api.core.servererrors.QueryValidationException;
+import com.datastax.oss.driver.api.core.servererrors.WriteTimeoutException;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.quarkus.security.UnauthorizedException;
 import io.stargate.sgv2.jsonapi.api.model.command.CommandResult;
 import io.stargate.sgv2.jsonapi.exception.JsonApiException;
 import jakarta.ws.rs.core.Response;
@@ -14,19 +22,18 @@ import java.util.function.Function;
  * implementation.
  */
 public final class ThrowableToErrorMapper {
-
   private static final BiFunction<Throwable, String, CommandResult.Error> MAPPER_WITH_MESSAGE =
       (throwable, message) -> {
         // if our own exception, shortcut
         if (throwable instanceof JsonApiException jae) {
           return jae.getCommandResultError(message);
         }
-        // Override response error code
+        // add error code as error field
+        Map<String, Object> fields = Map.of("exceptionClass", throwable.getClass().getSimpleName());
         if (throwable instanceof StatusRuntimeException sre) {
-          Map<String, Object> fields =
-              Map.of("exceptionClass", throwable.getClass().getSimpleName());
           if (sre.getStatus().getCode() == Status.Code.UNAUTHENTICATED) {
-            return new CommandResult.Error(message, fields, Response.Status.UNAUTHORIZED);
+            return new CommandResult.Error(
+                "UNAUTHENTICATED: Invalid token", fields, Response.Status.UNAUTHORIZED);
           } else if (sre.getStatus().getCode() == Status.Code.INTERNAL) {
             return new CommandResult.Error(message, fields, Response.Status.INTERNAL_SERVER_ERROR);
           } else if (sre.getStatus().getCode() == Status.Code.UNAVAILABLE) {
@@ -35,18 +42,33 @@ public final class ThrowableToErrorMapper {
             return new CommandResult.Error(message, fields, Response.Status.GATEWAY_TIMEOUT);
           }
         }
-        // add error code as error field
-        Map<String, Object> fields = Map.of("exceptionClass", throwable.getClass().getSimpleName());
+        if (throwable instanceof UnauthorizedException
+            || throwable
+                instanceof com.datastax.oss.driver.api.core.servererrors.UnauthorizedException) {
+          return new CommandResult.Error(
+              "UNAUTHENTICATED: Invalid token", fields, Response.Status.UNAUTHORIZED);
+        } else if (throwable instanceof QueryValidationException) {
+          if (message.contains("vector<float,")) { // TODO is there a better way?
+            message = "Mismatched vector dimension";
+          }
+          return new CommandResult.Error(message, fields, Response.Status.OK);
+        } else if (throwable instanceof NodeUnavailableException
+            || throwable instanceof DriverException
+            || throwable instanceof AllNodesFailedException
+            || throwable instanceof NoNodeAvailableException) {
+          return new CommandResult.Error(message, fields, Response.Status.INTERNAL_SERVER_ERROR);
+        } else if (throwable instanceof DriverTimeoutException
+            || throwable instanceof WriteTimeoutException) {
+          return new CommandResult.Error(message, fields, Response.Status.GATEWAY_TIMEOUT);
+        }
         return new CommandResult.Error(message, fields, Response.Status.OK);
       };
-
   private static final Function<Throwable, CommandResult.Error> MAPPER =
       throwable -> {
         String message = throwable.getMessage();
         if (message == null) {
           message = "Unexpected exception occurred.";
         }
-
         return MAPPER_WITH_MESSAGE.apply(throwable, message);
       };
 
