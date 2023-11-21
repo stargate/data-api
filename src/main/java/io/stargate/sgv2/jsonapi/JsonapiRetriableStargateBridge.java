dc5607b803d200cc683fca55c88d14fdcfebfcbd
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2023-11-21 16:25:57 -0500

    Return null

--- src/main/java/io/stargate/sgv2/jsonapi/JsonapiRetriableStargateBridge.java
+++ src/main/java/io/stargate/sgv2/jsonapi/JsonapiRetriableStargateBridge.java
@@ -1,12 +1,9 @@
 package io.stargate.sgv2.jsonapi;
 
 import io.quarkus.grpc.GrpcService;
-import io.stargate.sgv2.api.common.config.GrpcConfig;
 import io.stargate.sgv2.api.common.grpc.qualifier.Retriable;
-import io.stargate.sgv2.api.common.grpc.retries.GrpcRetryPredicate;
 import jakarta.annotation.Priority;
 import jakarta.enterprise.inject.Alternative;
-import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.Produces;
 
@@ -18,8 +15,7 @@ public class JsonapiRetriableStargateBridge {
 
   @Produces
   @Retriable
-  io.stargate.sgv2.api.common.grpc.RetriableStargateBridge retriableStargateBridge(
-      Instance<GrpcRetryPredicate> predicate, GrpcConfig grpcConfig) {
+  io.stargate.sgv2.api.common.grpc.RetriableStargateBridge retriableStargateBridge() {
     return null;
   }
 }
