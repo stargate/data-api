5e5eec7655fb3639aa762d5843f6e7d9e7eee64c
Hazel <hazel.he@datastax.com>
2023-11-21 14:30:56 -0500

    Reformat

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/DeleteManyIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/DeleteManyIntegrationTest.java
@@ -209,8 +209,8 @@ public class DeleteManyIntegrationTest extends AbstractCollectionIntegrationTest
     }
 
     /**
-     * Verify that DeleteMany with no filter will use truncate and delete all documents in a collection,
-     * instead of using Delete with maximum number to delete.
+     * Verify that DeleteMany with no filter will use truncate and delete all documents in a
+     * collection, instead of using Delete with maximum number to delete.
      */
     @Test
     public void noFilter() {
@@ -257,8 +257,8 @@ public class DeleteManyIntegrationTest extends AbstractCollectionIntegrationTest
     }
 
     /**
-     * Verify that DeleteMany with empty filter will use truncate and delete all documents in a collection,
-     * instead of using Delete with maximum number to delete.
+     * Verify that DeleteMany with empty filter will use truncate and delete all documents in a
+     * collection, instead of using Delete with maximum number to delete.
      */
     @Test
     public void emptyFilter() {
