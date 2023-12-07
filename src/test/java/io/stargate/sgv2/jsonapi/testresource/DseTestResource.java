3fb1519b4718c1bb71ff684cfedc1b4b9e91586d
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-12-07 14:04:05 -0800

    Change Stargate Coordinator Docker tag to use to make IT runs on IDE(A) work (#714)

--- src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
+++ src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
@@ -17,8 +17,10 @@ public class DseTestResource extends StargateTestResource {
     }
 
     if (null == System.getProperty("testing.containers.stargate-image")) {
+      // 07-Dec-2023, tatu: For some reason floating tag "v2.1" does not seem to work so
+      //    use specific version. Needs to be kept up to date:
       System.setProperty(
-          "testing.containers.stargate-image", "stargateio/coordinator-dse-next:v2.1");
+          "testing.containers.stargate-image", "stargateio/coordinator-dse-next:v2.1.0-BETA-6");
     }
 
     if (null == System.getProperty("testing.containers.cluster-persistence")) {
