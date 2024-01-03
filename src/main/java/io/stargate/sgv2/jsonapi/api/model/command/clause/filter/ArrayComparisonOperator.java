bde16daf7cb45a49cffdd8b617576ca78d6deced
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-01-03 16:19:33 -0500

    Moved the comment to java doc

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/clause/filter/ArrayComparisonOperator.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/clause/filter/ArrayComparisonOperator.java
@@ -1,10 +1,13 @@
 package io.stargate.sgv2.jsonapi.api.model.command.clause.filter;
 
-/** List of element level operator that can be used in Filter clause */
+/**
+ * List of element level operator that can be used in Filter clause NOTANY operator is used
+ * internally to support $not operation with $all
+ */
 public enum ArrayComparisonOperator implements FilterOperator {
   ALL("$all"),
   SIZE("$size"),
-  // Will not be supported from outside
+  /** Can not be used in filter clause operator in user api */
   NOTANY("$notany");
 
   private String operator;
