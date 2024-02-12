2ffa4d8b00c318a9e348b0d93e97778bff7fd736
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-02-12 11:45:38 -0500

    Refactored some pass through methods

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/Updatable.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/Updatable.java
@@ -2,6 +2,9 @@ package io.stargate.sgv2.jsonapi.api.model.command;
 
 import io.stargate.sgv2.jsonapi.api.model.command.clause.update.UpdateClause;
 
+/*
+ * All the commands that supports update clause will have to implement this.
+ */
 public interface Updatable {
   UpdateClause updateClause();
 }
--- src/main/java/io/stargate/sgv2/jsonapi/service/embedding/operation/EmbeddingService.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/embedding/operation/EmbeddingService.java
@@ -3,6 +3,10 @@ package io.stargate.sgv2.jsonapi.service.embedding.operation;
 import io.smallrye.mutiny.Uni;
 import java.util.List;
 
+/**
+ * Interface that accepts a list of texts that needs to be vectorized and returns embeddings based
+ * of chosen model.
+ */
 public interface EmbeddingService {
   Uni<List<float[]>> vectorize(List<String> texts);
 }
