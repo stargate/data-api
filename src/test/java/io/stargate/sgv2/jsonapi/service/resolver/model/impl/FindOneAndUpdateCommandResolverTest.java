95f16a2d201b985f8acbe6106e6e5f4e26a22602
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-02-12 09:24:44 -0500

    Removed the moved test cases

--- src/test/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/FindOneAndUpdateCommandResolverTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/FindOneAndUpdateCommandResolverTest.java
@@ -11,7 +11,6 @@ import io.stargate.sgv2.jsonapi.api.model.command.clause.update.UpdateClause;
 import io.stargate.sgv2.jsonapi.api.model.command.clause.update.UpdateOperator;
 import io.stargate.sgv2.jsonapi.api.model.command.impl.FindOneAndUpdateCommand;
 import io.stargate.sgv2.jsonapi.config.OperationsConfig;
-import io.stargate.sgv2.jsonapi.service.embedding.operation.TestEmbeddingService;
 import io.stargate.sgv2.jsonapi.service.operation.model.Operation;
 import io.stargate.sgv2.jsonapi.service.operation.model.ReadType;
 import io.stargate.sgv2.jsonapi.service.operation.model.impl.DBFilterBase;
@@ -234,76 +233,6 @@ public class FindOneAndUpdateCommandResolverTest {
                               .isEqualTo(filter);
                           assertThat(find.vector()).isNotNull();
                           assertThat(find.vector()).containsExactly(0.11f, 0.22f, 0.33f, 0.44f);
-                          assertThat(find.singleResponse()).isTrue();
-                        });
-              });
-    }
-
-    @Test
-    public void filterConditionVectorizeSearch() throws Exception {
-      String json =
-          """
-                    {
-                      "findOneAndUpdate": {
-                        "filter" : {"status" : "active"},
-                        "sort" : {"$vectorize" : "test data"},
-                        "update" : {"$set" : {"location" : "New York"}}
-                      }
-                    }
-                    """;
-
-      FindOneAndUpdateCommand command = objectMapper.readValue(json, FindOneAndUpdateCommand.class);
-      Operation operation =
-          resolver.resolveCommand(TestEmbeddingService.commandContextWithVectorize, command);
-
-      assertThat(operation)
-          .isInstanceOfSatisfying(
-              ReadAndUpdateOperation.class,
-              op -> {
-                assertThat(op.commandContext())
-                    .isEqualTo(TestEmbeddingService.commandContextWithVectorize);
-                assertThat(op.returnDocumentInResponse()).isTrue();
-                assertThat(op.returnUpdatedDocument()).isFalse();
-                assertThat(op.upsert()).isFalse();
-                assertThat(op.shredder()).isEqualTo(shredder);
-                assertThat(op.updateLimit()).isEqualTo(1);
-                assertThat(op.retryLimit()).isEqualTo(operationsConfig.lwt().retries());
-                assertThat(op.documentUpdater())
-                    .isInstanceOfSatisfying(
-                        DocumentUpdater.class,
-                        updater -> {
-                          UpdateClause updateClause =
-                              DocumentUpdaterUtils.updateClause(
-                                  UpdateOperator.SET,
-                                  objectMapper.createObjectNode().put("location", "New York"));
-
-                          assertThat(updater.updateOperations())
-                              .isEqualTo(updateClause.buildOperations());
-                        });
-                assertThat(op.findOperation())
-                    .isInstanceOfSatisfying(
-                        FindOperation.class,
-                        find -> {
-                          DBFilterBase.TextFilter filter =
-                              new DBFilterBase.TextFilter(
-                                  "status", DBFilterBase.MapFilterBase.Operator.EQ, "active");
-
-                          assertThat(find.objectMapper()).isEqualTo(objectMapper);
-                          assertThat(find.commandContext())
-                              .isEqualTo(TestEmbeddingService.commandContextWithVectorize);
-                          assertThat(find.pageSize()).isEqualTo(1);
-                          assertThat(find.limit()).isEqualTo(1);
-                          assertThat(find.pageState()).isNull();
-                          assertThat(find.readType()).isEqualTo(ReadType.DOCUMENT);
-                          assertThat(
-                                  find.logicalExpression()
-                                      .comparisonExpressions
-                                      .get(0)
-                                      .getDbFilters()
-                                      .get(0))
-                              .isEqualTo(filter);
-                          assertThat(find.vector()).isNotNull();
-                          assertThat(find.vector()).containsExactly(0.25f, 0.25f, 0.25f);
                           assertThat(find.singleResponse()).isTrue();
                         });
               });
