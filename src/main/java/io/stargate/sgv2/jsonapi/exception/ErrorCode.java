1492d0fc7cd9ba5c41b3af21e15c7b178cbee852
Tatu Saloranta <tatu.saloranta@datastax.com>
2024-03-04 14:30:00 -0800

    Fixes #898: improve error message for "multiple _id filters" case (#913)

--- src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCode.java
+++ src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCode.java
@@ -29,7 +29,7 @@ public enum ErrorCode {
   FILTER_UNRESOLVABLE("Unable to resolve the filter"),
 
   FILTER_MULTIPLE_ID_FILTER(
-      "Should only have one _id filter, document id cannot be restricted by more than one relation if it includes an Equal"),
+      "Cannot have more than one _id equals filter clause: use $in operator instead"),
 
   FILTER_FIELDS_LIMIT_VIOLATION("Filter fields size limitation violated"),
 
