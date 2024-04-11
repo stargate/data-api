ee988d731daae6aafdb756f4e0eefee5a451b060
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2024-04-11 13:53:07 -0400

    Removed unused exception class

--- src/main/java/io/stargate/sgv2/jsonapi/service/cqldriver/AlreadyInitializedException.java
+++
@@ -1,7 +0,0 @@
-package io.stargate.sgv2.jsonapi.service.cqldriver;
-
-public class AlreadyInitializedException extends RuntimeException {
-  public AlreadyInitializedException(String message) {
-    super(message);
-  }
-}
