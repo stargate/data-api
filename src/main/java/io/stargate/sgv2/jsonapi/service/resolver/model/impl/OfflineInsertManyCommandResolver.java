75692c40f09b0f7003dbb1add881d6a4aace2e3a
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2024-04-11 16:37:00 -0400

    offline mode flag fix

--- src/main/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/OfflineInsertManyCommandResolver.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/OfflineInsertManyCommandResolver.java
@@ -5,7 +5,6 @@ import io.stargate.sgv2.jsonapi.api.model.command.CommandContext;
 import io.stargate.sgv2.jsonapi.api.model.command.impl.OfflineInsertManyCommand;
 import io.stargate.sgv2.jsonapi.service.operation.model.Operation;
 import io.stargate.sgv2.jsonapi.service.operation.model.impl.InsertOperation;
-import io.stargate.sgv2.jsonapi.service.projection.IndexingProjector;
 import io.stargate.sgv2.jsonapi.service.resolver.model.CommandResolver;
 import io.stargate.sgv2.jsonapi.service.shredding.Shredder;
 import io.stargate.sgv2.jsonapi.service.shredding.model.WritableShreddedDocument;
@@ -16,14 +15,11 @@ import java.util.List;
 /** Resolves the {@link OfflineInsertManyCommand}. */
 @ApplicationScoped
 public class OfflineInsertManyCommandResolver implements CommandResolver<OfflineInsertManyCommand> {
-
   private final Shredder shredder;
-  private final ObjectMapper objectMapper;
 
   @Inject
   public OfflineInsertManyCommandResolver(Shredder shredder, ObjectMapper objectMapper) {
     this.shredder = shredder;
-    this.objectMapper = objectMapper;
   }
 
   @Override
@@ -33,13 +29,12 @@ public class OfflineInsertManyCommandResolver implements CommandResolver<Offline
 
   @Override
   public Operation resolveCommand(CommandContext ctx, OfflineInsertManyCommand command) {
-    final IndexingProjector projection = ctx.indexingProjector();
     final List<WritableShreddedDocument> shreddedDocuments =
         command.documents().stream().map(doc -> shredder.shred(ctx, doc, null)).toList();
 
     // Offline insert is always ordered
     boolean ordered = true;
 
-    return new InsertOperation(ctx, shreddedDocuments, ordered);
+    return new InsertOperation(ctx, shreddedDocuments, ordered, true);
   }
 }
