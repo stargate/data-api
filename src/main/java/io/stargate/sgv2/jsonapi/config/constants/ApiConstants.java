882d1dcf853dc34e0e7935851d193ac7eee2d7b2
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2024-04-29 14:19:05 -0400

    Javadoc update

--- src/main/java/io/stargate/sgv2/jsonapi/config/constants/ApiConstants.java
+++ src/main/java/io/stargate/sgv2/jsonapi/config/constants/ApiConstants.java
@@ -1,5 +1,12 @@
 package io.stargate.sgv2.jsonapi.config.constants;
 
+/**
+ * This configuration is maintained outside the Quarkus smallrye config system.
+ *
+ * <p>This is because, the `isOffline` function here is used to decide on how to load the properties
+ * from <code> DebugModeConfig</code> via the SmallRye config library in <code>JsonApiException
+ * </code> class. So, this property itself can not be loaded from SmallRye config.
+ */
 public class ApiConstants {
   public static final String OFFLINE_WRITER_MODE_PROPERTY = "stargate.offline.sstablewriter";
 
