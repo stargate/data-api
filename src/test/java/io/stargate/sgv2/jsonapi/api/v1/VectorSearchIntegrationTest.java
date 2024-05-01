fd742850698063f4477d5b9ff35575cd0f9f8a46
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2024-04-30 20:48:16 -0400

    DSE error message changes

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/VectorSearchIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/VectorSearchIntegrationTest.java
@@ -974,7 +974,7 @@ public class VectorSearchIntegrationTest extends AbstractNamespaceIntegrationTes
           .body("errors[0].errorCode", is("INVALID_QUERY"))
           .body(
               "errors[0].message",
-              endsWith("Zero vectors cannot be indexed or queried with cosine similarity"));
+              endsWith("Zero and near-zero vectors cannot be indexed or queried with cosine similarity"));
     }
 
     @Test
