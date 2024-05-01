9d07a1b43a04df90967540700a0fb5e275b0f25f
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2024-05-01 16:28:02 -0400

    Formatting fix

--- src/main/offline/io/stargate/sgv2/jsonapi/api/request/FileWriterParams.java
+++ src/main/offline/io/stargate/sgv2/jsonapi/api/request/FileWriterParams.java
@@ -1,29 +1,29 @@
 package io.stargate.sgv2.jsonapi.api.request;
 
-import com.datastax.oss.driver.api.core.CqlSession;
 
-import java.util.List;
 import io.stargate.sgv2.jsonapi.service.cqldriver.sstablewriter.FileWriterSession;
+import java.util.List;
 
 /**
- * The parameters that are required to write the data to SSTable files used primarily by {@link FileWriterSession}
+ * The parameters that are required to write the data to SSTable files used primarily by {@link
+ * FileWriterSession}
  */
 public record FileWriterParams(
-        /* The name of the keyspace */
+    /* The name of the keyspace */
     String keyspaceName,
-        /* The name of the table */
+    /* The name of the table */
     String tableName,
-        /* The directory where the SSTable files will be written */
+    /* The directory where the SSTable files will be written */
     String ssTableOutputDirectory,
-        /* The size of the buffer used to write the SSTable files */
+    /* The size of the buffer used to write the SSTable files */
     int fileWriterBufferSizeInMB,
-        /* The CQL to create the table */
+    /* The CQL to create the table */
     String createTableCQL,
-        /* The CQL to insert the data into the table */
+    /* The CQL to insert the data into the table */
     String insertStatementCQL,
-        /* The CQL to create the indexes */
+    /* The CQL to create the indexes */
     List<String> indexCQLs,
-        /* The flag to enable vector */
+    /* The flag to enable vector */
     Boolean vectorEnabled) {
   public FileWriterParams {
     if (keyspaceName == null || keyspaceName.isBlank()) {
