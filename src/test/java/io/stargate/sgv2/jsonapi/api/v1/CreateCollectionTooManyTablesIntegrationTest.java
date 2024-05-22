9729ac820fe78c3e77f11be4c4e8feb51c35bc76
Mahesh Rajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-05-22 13:05:56 -0400

    Changes to improve CI timing - Fix 2 (#1109)

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/CreateCollectionTooManyTablesIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/CreateCollectionTooManyTablesIntegrationTest.java
@@ -9,8 +9,6 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.http.ContentType;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
 import org.junit.jupiter.api.ClassOrderer;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
 
@@ -42,38 +40,23 @@ class CreateCollectionTooManyTablesIntegrationTest extends AbstractNamespaceInte
     }
   }
 
-  @Nested
-  @Order(1)
-  class TooManyCollections {
-    @Test
-    public void enforceMaxCollections() {
-      // Don't use auto-generated namespace that rest of the test uses
-      final String NS = "ns_too_many_collections";
-      createNamespace(NS);
-      final String createTemplate =
-          """
-              {
-                "createCollection": {
-                  "name": "tooMany_%d"
+  @Test
+  public void enforceMaxCollections() {
+    // Don't use auto-generated namespace that rest of the test uses
+    final String NS = "ns_too_many_collections";
+    createNamespace(NS);
+    final String createTemplate =
+        """
+                {
+                  "createCollection": {
+                    "name": "tooMany_%d"
+                  }
                 }
-              }
-              """;
+                """;
 
-      // First create maximum number of collections
-      for (int i = 1; i <= COLLECTIONS_TO_CREATE; ++i) {
-        String json = createTemplate.formatted(i);
-        given()
-            .headers(getHeaders())
-            .contentType(ContentType.JSON)
-            .body(json)
-            .when()
-            .post(NamespaceResource.BASE_PATH, NS)
-            .then()
-            .statusCode(200)
-            .body("status.ok", is(1));
-      }
-      // And then failure
-      String json = createTemplate.formatted(99);
+    // First create maximum number of collections
+    for (int i = 1; i <= COLLECTIONS_TO_CREATE; ++i) {
+      String json = createTemplate.formatted(i);
       given()
           .headers(getHeaders())
           .contentType(ContentType.JSON)
@@ -82,29 +65,40 @@ class CreateCollectionTooManyTablesIntegrationTest extends AbstractNamespaceInte
           .post(NamespaceResource.BASE_PATH, NS)
           .then()
           .statusCode(200)
-          .body("status", is(nullValue()))
-          .body("data", is(nullValue()))
-          .body(
-              "errors[0].message",
-              is(
-                  "Too many collections: number of collections in database cannot exceed "
-                      + COLLECTIONS_TO_CREATE
-                      + ", already have "
-                      + COLLECTIONS_TO_CREATE))
-          .body("errors[0].errorCode", is("TOO_MANY_COLLECTIONS"))
-          .body("errors[0].exceptionClass", is("JsonApiException"));
-
-      // But then verify that re-creating an existing one should still succeed
-      // (if using same settings)
-      given()
-          .headers(getHeaders())
-          .contentType(ContentType.JSON)
-          .body(createTemplate.formatted(1))
-          .when()
-          .post(NamespaceResource.BASE_PATH, NS)
-          .then()
-          .statusCode(200)
           .body("status.ok", is(1));
     }
+    // And then failure
+    String json = createTemplate.formatted(99);
+    given()
+        .headers(getHeaders())
+        .contentType(ContentType.JSON)
+        .body(json)
+        .when()
+        .post(NamespaceResource.BASE_PATH, NS)
+        .then()
+        .statusCode(200)
+        .body("status", is(nullValue()))
+        .body("data", is(nullValue()))
+        .body(
+            "errors[0].message",
+            is(
+                "Too many collections: number of collections in database cannot exceed "
+                    + COLLECTIONS_TO_CREATE
+                    + ", already have "
+                    + COLLECTIONS_TO_CREATE))
+        .body("errors[0].errorCode", is("TOO_MANY_COLLECTIONS"))
+        .body("errors[0].exceptionClass", is("JsonApiException"));
+
+    // But then verify that re-creating an existing one should still succeed
+    // (if using same settings)
+    given()
+        .headers(getHeaders())
+        .contentType(ContentType.JSON)
+        .body(createTemplate.formatted(1))
+        .when()
+        .post(NamespaceResource.BASE_PATH, NS)
+        .then()
+        .statusCode(200)
+        .body("status.ok", is(1));
   }
 }
