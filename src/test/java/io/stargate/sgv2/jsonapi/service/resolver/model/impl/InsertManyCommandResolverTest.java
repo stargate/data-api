abb7a944076ae9dfd366f0e6d48e7682cfb32c2e
Tatu Saloranta <tatu.saloranta@datastax.com>
2024-06-20 20:07:28 -0700

    Remove useless test

--- src/test/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/InsertManyCommandResolverTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/resolver/model/impl/InsertManyCommandResolverTest.java
@@ -1,7 +1,6 @@
 package io.stargate.sgv2.jsonapi.service.resolver.model.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.InjectMock;
@@ -10,8 +9,6 @@ import io.quarkus.test.junit.TestProfile;
 import io.stargate.sgv2.jsonapi.api.model.command.CommandContext;
 import io.stargate.sgv2.jsonapi.api.model.command.impl.InsertManyCommand;
 import io.stargate.sgv2.jsonapi.api.request.DataApiRequestInfo;
-import io.stargate.sgv2.jsonapi.exception.ErrorCode;
-import io.stargate.sgv2.jsonapi.exception.JsonApiException;
 import io.stargate.sgv2.jsonapi.service.operation.model.Operation;
 import io.stargate.sgv2.jsonapi.service.operation.model.impl.InsertOperation;
 import io.stargate.sgv2.jsonapi.service.shredding.Shredder;
@@ -171,31 +168,6 @@ public class InsertManyCommandResolverTest {
                 assertThat(op.ordered()).isTrue();
                 assertThat(op.insertions()).hasSize(2);
               });
-    }
-
-    @Test
-    public void shredderFailure() throws Exception {
-      String json =
-          """
-          {
-            "insertMany": {
-              "documents": [
-                {
-                  "_id": "1",
-                  "location": "London"
-                },
-                "primitive"
-              ]
-            }
-          }
-          """;
-
-      InsertManyCommand command = objectMapper.readValue(json, InsertManyCommand.class);
-      Throwable failure = catchThrowable(() -> resolver.resolveCommand(commandContext, command));
-
-      assertThat(failure)
-          .isInstanceOf(JsonApiException.class)
-          .hasFieldOrPropertyWithValue("errorCode", ErrorCode.SHRED_BAD_DOCUMENT_TYPE);
     }
   }
 }
