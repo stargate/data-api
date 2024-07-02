0d0ce4e530085aba11b94d6ef10036a629b502e8
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-07-02 10:22:03 -0400

    Added comments

--- src/main/java/io/stargate/sgv2/jsonapi/api/request/EmbeddingCredential.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/request/EmbeddingCredential.java
@@ -2,5 +2,13 @@ package io.stargate.sgv2.jsonapi.api.request;
 
 import java.util.Optional;
 
+/**
+ * EmbeddingCredential is a record that holds the embedding provider credentials for the embedding
+ * service passed as header.
+ *
+ * @param apiKey - API token for the embedding service
+ * @param accessId - Access Id used for AWS Bedrock embedding service
+ * @param secretId - Secret Id used for AWS Bedrock embedding service
+ */
 public record EmbeddingCredential(
     Optional<String> apiKey, Optional<String> accessId, Optional<String> secretId) {}
