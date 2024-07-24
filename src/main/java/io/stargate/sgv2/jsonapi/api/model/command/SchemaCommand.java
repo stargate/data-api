59a69ec74ed595642b049309b452968ff133acd5
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-07-24 22:59:25 +0530

    Fixed the error message for invalid SchemaCommands and the it's IT.

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/SchemaCommand.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/SchemaCommand.java
@@ -1,4 +1,12 @@
 package io.stargate.sgv2.jsonapi.api.model.command;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /** Interface for all commands executed for schema related command for both Collection and Table. */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = NamespaceCommand.class),
+  @JsonSubTypes.Type(value = KeyspaceCommand.class)
+})
 public interface SchemaCommand extends Command {}
