0fc2011bbb7f38e7f85400c9a1480a95f5cb9cc6
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-07-31 10:35:48 +0530

    Added back the drop table command

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/TableOnlyCommand.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/TableOnlyCommand.java
@@ -3,8 +3,12 @@ package io.stargate.sgv2.jsonapi.api.model.command;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.stargate.sgv2.jsonapi.api.model.command.impl.CreateTableCommand;
+import io.stargate.sgv2.jsonapi.api.model.command.impl.DropTableCommand;
 
 /** Interface for all commands executed against a keyspace. */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
-@JsonSubTypes({@JsonSubTypes.Type(value = CreateTableCommand.class)})
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = CreateTableCommand.class),
+  @JsonSubTypes.Type(value = DropTableCommand.class)
+})
 public interface TableOnlyCommand extends NamespaceCommand {}
