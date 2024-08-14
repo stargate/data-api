5f74b06b26208f2906b66925abd83e3b253b161c
Mahesh Rajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-08-14 17:08:40 -0400

    Added support for addIndex and dropIndex commands (#1322)
    Co-authored-by: Tatu Saloranta <tatu.saloranta@datastax.com>

---
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/impl/AddIndexCommand.java
@@ -0,0 +1,24 @@
+package io.stargate.sgv2.jsonapi.api.model.command.impl;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.stargate.sgv2.jsonapi.api.model.command.CollectionCommand;
+import io.stargate.sgv2.jsonapi.api.model.command.NoOptionsCommand;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(description = "Command that creates an index for a column in a table.")
+@JsonTypeName("addIndex")
+public record AddIndexCommand(
+    @NotNull
+        @Size(min = 1, max = 48)
+        @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9_]*")
+        @Schema(description = "Name of the column to create the index on")
+        String column,
+    @NotNull
+        @Size(min = 1, max = 48)
+        @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9_]*")
+        @Schema(description = "Unique name for the index.")
+        String indexName)
+    implements NoOptionsCommand, CollectionCommand {}
