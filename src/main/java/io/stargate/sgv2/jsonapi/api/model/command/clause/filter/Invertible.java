b2e42050c178f1990aab6500d30afc32ca0cdbab
YuqiDu <istimdu@gmail.com>
2024-08-16 15:35:38 -0700

    Revert "init" (#1345)

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/clause/filter/Invertible.java
+++
@@ -1,8 +0,0 @@
-package io.stargate.sgv2.jsonapi.api.model.command.clause.filter;
-
-/** Interface Invertible implemented by comparisonExpression and logicalExpression */
-public interface Invertible {
-
-  /** invert method to invert a comparisonExpression or logicalExpression */
-  Object invert();
-}
