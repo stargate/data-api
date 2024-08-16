e5ffee0d0b5192a64c64826e6ab83d1a04ef7771
Yuqi Du <istimdu@gmail.com>
2024-08-16 15:38:13 -0700

    format

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/clause/filter/Invertible.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/clause/filter/Invertible.java
@@ -3,6 +3,6 @@ package io.stargate.sgv2.jsonapi.api.model.command.clause.filter;
 /** Interface Invertible implemented by comparisonExpression and logicalExpression */
 public interface Invertible {
 
-  /** invert method to invert a comparisonExpression or logicalExpression */
+  /** method to invert a comparisonExpression or logicalExpression */
   Object invert();
 }
