3b56953ea5c1b4882654da16f7cbb7466cc5929d
Tatu Saloranta <tatu.saloranta@datastax.com>
2024-08-22 14:43:04 -0700

    More tests

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/FindOneTableIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/FindOneTableIntegrationTest.java
@@ -1,11 +1,19 @@
 package io.stargate.sgv2.jsonapi.api.v1.tables;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.http.ContentType;
+import io.stargate.sgv2.jsonapi.api.v1.CollectionResource;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
-import java.io.IOException;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
@@ -16,51 +24,104 @@ import org.junit.jupiter.api.TestClassOrder;
 @WithTestResource(value = DseTestResource.class, restrictToAnnotatedClass = false)
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
 public class FindOneTableIntegrationTest extends AbstractTableIntegrationTestBase {
-  private static final ObjectMapper MAPPER = new ObjectMapper();
+  static final String TABLE_WITH_STRING_ID_AGE_NAME = "findOneSingleStringKeyTable";
+
+  @BeforeAll
+  public final void createDefaultTables() {
+    createTableWithColumns(
+        TABLE_WITH_STRING_ID_AGE_NAME,
+        Map.of(
+            "id",
+            Map.of("type", "text"),
+            "age",
+            Map.of("type", "int"),
+            "name",
+            Map.of("type", "text")),
+        "id");
+  }
 
   @Nested
   @Order(1)
   class FindOneSuccess {
     @Test
+    // Must be run first, before any other test
     @Order(1)
+    public void findOnEmpty() {
+      given()
+          .headers(getHeaders())
+          .contentType(ContentType.JSON)
+          .body(
+              """
+                  {
+                    "findOne": { }
+                  }
+              """)
+          .when()
+          .post(CollectionResource.BASE_PATH, namespaceName, TABLE_WITH_STRING_ID_AGE_NAME)
+          .then()
+          .statusCode(200)
+          .body("data.document", is(nullValue()))
+          .body("status", is(nullValue()))
+          .body("errors", is(nullValue()));
+    }
+
+    @Test
+    @Order(2)
     public void findOneSingleStringKey() {
-      createTableWithColumns(
-          "findOneSingleStringKeyTable",
-          Map.of(
-              "id",
-              Map.of("type", "text"),
-              "age",
-              Map.of("type", "int"),
-              "name",
-              Map.of("type", "text")),
-          "id");
+      // First, insert 2 documents:
+      insertOneInTable(
+          TABLE_WITH_STRING_ID_AGE_NAME,
+          """
+                      {
+                          "id": "a",
+                          "age": 20,
+                          "name": "John"
+                      }
+                      """);
+      insertOneInTable(
+          TABLE_WITH_STRING_ID_AGE_NAME,
+          """
+                      {
+                          "id": "b",
+                          "age": 40,
+                          "name": "Bob"
+                      }
+                      """);
     }
   }
 
   @Nested
   @Order(2)
-  class FindOneFail {}
-
-  void createTableWithColumns(String tableName, Map<String, Object> columns, Object primaryKeyDef) {
-    try {
-      createTable(
+  class FindOneFail {
+    @Test
+    @Order(1)
+    public void failOnUnknownColumn() {
+      given()
+          .headers(getHeaders())
+          .contentType(ContentType.JSON)
+          .body(
               """
                           {
-                              "createTable": {
-                                  "name": "%s",
-                                  "definition": {
-                                      "columns": %s,
-                                      "primaryKey": %s
-                                  }
+                            "findOne": {
+                              "filter": {
+                                "unknown": "a"
                               }
+                            }
                           }
-                    """
-              .formatted(
-                  tableName,
-                  MAPPER.writeValueAsString(columns),
-                  MAPPER.writeValueAsString(primaryKeyDef)));
-    } catch (IOException e) { // should never happen
-      throw new RuntimeException(e);
+                      """)
+          .when()
+          .post(CollectionResource.BASE_PATH, namespaceName, TABLE_WITH_STRING_ID_AGE_NAME)
+          .then()
+          // Not like it should be, but until fixed let's verify current behavior
+          .statusCode(500)
+          .body("data", is(nullValue()))
+          .body("errors", is(notNullValue()))
+          .body("errors", hasSize(1))
+          .body("errors[0].exceptionClass", is("JsonApiException"))
+          .body("errors[0].errorCode", is("SERVER_UNHANDLED_ERROR"))
+          .body(
+              "errors[0].message",
+              containsString("FilterClause Clause does not support validating for Tables"));
     }
   }
 }
