255dbaeb8b471a646247ab30815182dfab61f7f4
Tatu Saloranta <tatu.saloranta@datastax.com>
2024-08-30 16:50:01 -0700

    Fix a copy-paste error

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/util/DataApiNamespaceCommandSender.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/util/DataApiNamespaceCommandSender.java
@@ -1,7 +1,7 @@
 package io.stargate.sgv2.jsonapi.api.v1.util;
 
 import io.restassured.specification.RequestSpecification;
-import io.stargate.sgv2.jsonapi.api.v1.CollectionResource;
+import io.stargate.sgv2.jsonapi.api.v1.NamespaceResource;
 
 public class DataApiNamespaceCommandSender
     extends DataApiCommandSenderBase<DataApiNamespaceCommandSender> {
@@ -10,7 +10,7 @@ public class DataApiNamespaceCommandSender
   }
 
   protected io.restassured.response.Response postInternal(RequestSpecification request) {
-    return request.post(CollectionResource.BASE_PATH, namespace);
+    return request.post(NamespaceResource.BASE_PATH, namespace);
   }
 
   public DataApiResponseValidator postCreateTable(String tableDefAsJSON) {
