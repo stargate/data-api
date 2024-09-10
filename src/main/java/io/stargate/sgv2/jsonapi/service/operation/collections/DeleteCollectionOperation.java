24c2e4c2dae4b72984e30580099a32579a753698
Tatu Saloranta <tatu.saloranta@datastax.com>
2024-09-10 15:23:17 -0700

    Minor refactoring: hide type, name fields of SchemaObject

--- src/main/java/io/stargate/sgv2/jsonapi/service/operation/collections/DeleteCollectionOperation.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/operation/collections/DeleteCollectionOperation.java
@@ -167,8 +167,8 @@ public record DeleteCollectionOperation(
     String delete = "DELETE FROM \"%s\".\"%s\" WHERE key = ? IF tx_id = ?";
     return String.format(
         delete,
-        commandContext.schemaObject().name.keyspace(),
-        commandContext.schemaObject().name.table());
+        commandContext.schemaObject().name().keyspace(),
+        commandContext.schemaObject().name().table());
   }
 
   /**
