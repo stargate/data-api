f214c52193b84833d8a66a4d64f7b2b0518104c8
Hazel <hazel.he@datastax.com>
2024-09-10 12:23:54 -0400

    add unit tests

--- src/test/java/io/stargate/sgv2/jsonapi/service/cqldriver/executor/DefaultDriverExceptionHandlerTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/cqldriver/executor/DefaultDriverExceptionHandlerTest.java
@@ -3,10 +3,15 @@ package io.stargate.sgv2.jsonapi.service.cqldriver.executor;
 import static io.stargate.sgv2.jsonapi.exception.ErrorFormatters.errFmt;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
 
+import com.datastax.oss.driver.api.core.DefaultConsistencyLevel;
 import com.datastax.oss.driver.api.core.DriverException;
 import com.datastax.oss.driver.api.core.connection.ClosedConnectionException;
+import com.datastax.oss.driver.api.core.servererrors.*;
+import com.datastax.oss.driver.internal.core.metadata.DefaultNode;
 import io.stargate.sgv2.jsonapi.exception.DatabaseException;
+import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -70,12 +75,52 @@ public class DefaultDriverExceptionHandlerTest {
   }
 
   private static Stream<Arguments> tableDriverErrorHandledData() {
+    DefaultNode mockCoordinator = mock(DefaultNode.class);
     return Stream.of(
+        // Test for CLOSED_CONNECTION
         Arguments.of(
             new ClosedConnectionException("closed"),
             DatabaseException.Code.CLOSED_CONNECTION.name(),
             true,
             true,
-            true));
+            true),
+        // Test for READ_FAILURE
+        Arguments.of(
+            new ReadFailureException(
+                mockCoordinator, DefaultConsistencyLevel.LOCAL_QUORUM, 1, 2, 1, true, Map.of()),
+            DatabaseException.Code.READ_FAILURE.name(),
+            true,
+            true,
+            false),
+        // Test for READ_TIMEOUT
+        Arguments.of(
+            new ReadTimeoutException(
+                mockCoordinator, DefaultConsistencyLevel.LOCAL_QUORUM, 1, 2, true),
+            DatabaseException.Code.READ_TIMEOUT.name(),
+            true,
+            true,
+            false),
+        // Test for WRITE_FAILURE
+        Arguments.of(
+            new WriteFailureException(
+                mockCoordinator,
+                DefaultConsistencyLevel.LOCAL_QUORUM,
+                1,
+                2,
+                WriteType.SIMPLE,
+                1,
+                Map.of()),
+            DatabaseException.Code.WRITE_FAILURE.name(),
+            true,
+            true,
+            false),
+        // Test for WRITE_TIMEOUT
+        Arguments.of(
+            new WriteTimeoutException(
+                mockCoordinator, DefaultConsistencyLevel.LOCAL_QUORUM, 1, 2, WriteType.SIMPLE),
+            DatabaseException.Code.WRITE_TIMEOUT.name(),
+            true,
+            true,
+            false));
   }
 }
