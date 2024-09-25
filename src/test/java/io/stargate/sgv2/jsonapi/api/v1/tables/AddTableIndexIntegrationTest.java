1025d01f61606e55a4bd99687013a2ab2b4a6c8c
Tatu Saloranta <tatu.saloranta@datastax.com>
2024-09-25 15:26:11 -0700

    Refactor code to use new IT assertion types

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/AddTableIndexIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/AddTableIndexIntegrationTest.java
@@ -1,13 +1,11 @@
 package io.stargate.sgv2.jsonapi.api.v1.tables;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.containsString;
 
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import io.restassured.http.ContentType;
-import io.stargate.sgv2.jsonapi.api.v1.CollectionResource;
+import io.stargate.sgv2.jsonapi.api.v1.util.DataApiCommandSenders;
+import io.stargate.sgv2.jsonapi.exception.ErrorCodeV1;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
 import java.util.Map;
 import org.junit.jupiter.api.*;
@@ -39,23 +37,16 @@ class AddTableIndexIntegrationTest extends AbstractTableIntegrationTestBase {
     @Test
     @Order(1)
     public void addIndex() {
-      String json =
-          """
-                      {
-                          "addIndex": {
-                              "column": "age",
-                              "indexName": "age_index"
-                          }
-                      }
-                      """;
-      given()
-          .headers(getHeaders())
-          .contentType(ContentType.JSON)
-          .body(json)
-          .when()
-          .post(CollectionResource.BASE_PATH, keyspaceName, simpleTableName)
-          .then()
-          .statusCode(200)
+      DataApiCommandSenders.assertTableCommand(keyspaceName, simpleTableName)
+          .postCommand(
+              "addIndex",
+              """
+                  {
+                          "column": "age",
+                          "indexName": "age_index"
+                  }
+                  """)
+          .hasNoErrors()
           .body("status.ok", is(1));
     }
   }
@@ -64,29 +55,17 @@ class AddTableIndexIntegrationTest extends AbstractTableIntegrationTestBase {
   @Order(2)
   class AddIndexFailure {
     @Test
-    public void addIndex() {
-      String json =
-          """
+    public void tryAddIndexMissingColumn() {
+      DataApiCommandSenders.assertTableCommand(keyspaceName, simpleTableName)
+          .postCommand(
+              "addIndex",
+              """
                       {
-                          "addIndex": {
                               "column": "city",
                               "indexName": "city_index"
-                          }
                       }
-                      """;
-      given()
-          .headers(getHeaders())
-          .contentType(ContentType.JSON)
-          .body(json)
-          .when()
-          .post(CollectionResource.BASE_PATH, keyspaceName, simpleTableName)
-          .then()
-          .statusCode(200)
-          .body("errors", is(notNullValue()))
-          .body("errors", hasSize(1))
-          .body("errors[0].exceptionClass", is("JsonApiException"))
-          .body("errors[0].errorCode", is("INVALID_QUERY"))
-          .body("errors[0].message", containsString("Undefined column name city"));
+                      """)
+          .hasSingleApiError(ErrorCodeV1.INVALID_QUERY, "Undefined column name city");
     }
   }
 }
