9a2b13670f88368d6fb597444a0fa3529e1d3625
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-10-07 20:30:25 -0400

    Rebased to main

--- src/main/java/io/stargate/sgv2/jsonapi/service/cqldriver/executor/TableSchemaObject.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/cqldriver/executor/TableSchemaObject.java
@@ -2,15 +2,26 @@ package io.stargate.sgv2.jsonapi.service.cqldriver.executor;
 
 import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.data.ByteUtils;
+import com.datastax.oss.driver.api.core.metadata.schema.ClusteringOrder;
 import com.datastax.oss.driver.api.core.metadata.schema.ColumnMetadata;
 import com.datastax.oss.driver.api.core.metadata.schema.IndexMetadata;
 import com.datastax.oss.driver.api.core.metadata.schema.TableMetadata;
+import com.datastax.oss.driver.api.core.type.MapType;
 import com.datastax.oss.driver.api.core.type.VectorType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.stargate.sgv2.jsonapi.api.model.command.impl.VectorizeConfig;
+import io.stargate.sgv2.jsonapi.api.model.command.table.definition.PrimaryKey;
+import io.stargate.sgv2.jsonapi.api.model.command.table.definition.datatype.ColumnType;
+import io.stargate.sgv2.jsonapi.api.model.command.table.definition.datatype.ComplexTypes;
+import io.stargate.sgv2.jsonapi.api.model.command.table.definition.datatype.PrimitiveTypes;
 import io.stargate.sgv2.jsonapi.exception.SchemaException;
 import io.stargate.sgv2.jsonapi.service.schema.SimilarityFunction;
+import io.stargate.sgv2.jsonapi.service.schema.tables.ApiDataTypeDef;
+import io.stargate.sgv2.jsonapi.service.schema.tables.ApiDataTypeDefs;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -20,6 +31,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class TableSchemaObject extends TableBasedSchemaObject {
 
@@ -33,7 +45,7 @@ public class TableSchemaObject extends TableBasedSchemaObject {
   }
 
   @Override
-   public VectorConfig vectorConfig() {
+  public VectorConfig vectorConfig() {
     return vectorConfig;
   }
 
@@ -114,5 +126,112 @@ public class TableSchemaObject extends TableBasedSchemaObject {
       vectorConfig = new VectorConfig(true, Collections.unmodifiableList(columnVectorDefinitions));
     }
     return new TableSchemaObject(tableMetadata, vectorConfig);
+  }
+
+  public TableResponse toTableResponse() {
+    String tableName = name().table();
+    HashMap<String, ColumnType> columnsDefinition = new HashMap<>();
+    for (Map.Entry<CqlIdentifier, ColumnMetadata> column :
+        tableMetadata().getColumns().entrySet()) {
+      ColumnType type = getColumnType(column.getKey().asInternal(), column.getValue());
+      columnsDefinition.put(column.getKey().asInternal(), type);
+    }
+
+    final List<String> partitionBy =
+        tableMetadata().getPartitionKey().stream()
+            .map(column -> column.getName().asInternal())
+            .collect(Collectors.toList());
+    final List<PrimaryKey.OrderingKey> partitionSort =
+        tableMetadata().getClusteringColumns().entrySet().stream()
+            .map(
+                entry ->
+                    new PrimaryKey.OrderingKey(
+                        entry.getKey().getName().asInternal(),
+                        entry.getValue() == ClusteringOrder.ASC
+                            ? PrimaryKey.OrderingKey.Order.ASC
+                            : PrimaryKey.OrderingKey.Order.DESC))
+            .collect(Collectors.toList());
+    PrimaryKey primaryKey =
+        new PrimaryKey(
+            partitionBy.toArray(new String[0]),
+            partitionSort.toArray(new PrimaryKey.OrderingKey[0]));
+    return new TableResponse(
+        tableName, new TableResponse.TableDefinition(columnsDefinition, primaryKey));
+  }
+
+  // Need to handle frozen types
+  private ColumnType getColumnType(String columnName, ColumnMetadata columnMetadata) {
+    if (columnMetadata.getType() instanceof VectorType vt) {
+      // Schema will always have VectorConfig for vector type
+      VectorConfig.ColumnVectorDefinition columnVectorDefinition =
+          vectorConfig.columnVectorDefinitions().stream()
+              .filter(vc -> vc.fieldName().equals(columnName))
+              .findFirst()
+              .get();
+      VectorizeConfig vectorizeConfig =
+          columnVectorDefinition.vectorizeConfig() == null
+              ? null
+              : new VectorizeConfig(
+                  columnVectorDefinition.vectorizeConfig().provider(),
+                  columnVectorDefinition.vectorizeConfig().modelName(),
+                  columnVectorDefinition.vectorizeConfig().authentication(),
+                  columnVectorDefinition.vectorizeConfig().parameters());
+      return new ComplexTypes.VectorType(PrimitiveTypes.FLOAT, vt.getDimensions(), vectorizeConfig);
+    } else if (columnMetadata.getType() instanceof MapType mt) {
+      if (!mt.isFrozen()) {
+        final Optional<ApiDataTypeDef> apiDataTypeDefKey = ApiDataTypeDefs.from(mt.getKeyType());
+        final Optional<ApiDataTypeDef> apiDataTypeDefValue =
+            ApiDataTypeDefs.from(mt.getValueType());
+        if (apiDataTypeDefKey.isPresent() && apiDataTypeDefValue.isPresent()) {
+          return new ComplexTypes.MapType(
+              PrimitiveTypes.fromString(apiDataTypeDefKey.get().getApiType().getApiName()),
+              PrimitiveTypes.fromString(apiDataTypeDefValue.get().getApiType().getApiName()));
+        }
+      }
+      // return unsupported format
+      return new ComplexTypes.UnsupportedType(mt.asCql(true, false));
+
+    } else if (columnMetadata.getType()
+        instanceof com.datastax.oss.driver.api.core.type.ListType lt) {
+      if (!lt.isFrozen()) {
+        final Optional<ApiDataTypeDef> apiDataTypeDef = ApiDataTypeDefs.from(lt.getElementType());
+        if (apiDataTypeDef.isPresent()) {
+          return new ComplexTypes.ListType(
+              PrimitiveTypes.fromString(apiDataTypeDef.get().getApiType().getApiName()));
+        }
+      }
+      // return unsupported format
+      return new ComplexTypes.UnsupportedType(lt.asCql(true, false));
+
+    } else if (columnMetadata.getType()
+        instanceof com.datastax.oss.driver.api.core.type.SetType st) {
+      if (!st.isFrozen()) {
+        final Optional<ApiDataTypeDef> apiDataTypeDef = ApiDataTypeDefs.from(st.getElementType());
+        if (apiDataTypeDef.isPresent()) {
+          return new ComplexTypes.SetType(
+              PrimitiveTypes.fromString(apiDataTypeDef.get().getApiType().getApiName()));
+        }
+      }
+      // return unsupported format
+      return new ComplexTypes.UnsupportedType(st.asCql(true, false));
+    } else {
+      final Optional<ApiDataTypeDef> apiDataTypeDef =
+          ApiDataTypeDefs.from(columnMetadata.getType());
+      if (apiDataTypeDef.isPresent())
+        return PrimitiveTypes.fromString(apiDataTypeDef.get().getApiType().getApiName());
+      else {
+        // Need to return unsupported type
+        throw new RuntimeException("Unknown data type: " + columnMetadata.getType());
+      }
+    }
+  }
+
+  @JsonPropertyOrder({"name", "definition"})
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  public record TableResponse(String name, TableDefinition definition) {
+
+    @JsonPropertyOrder({"columns", "primaryKey"})
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    record TableDefinition(Map<String, ColumnType> columns, PrimaryKey primaryKey) {}
   }
 }
