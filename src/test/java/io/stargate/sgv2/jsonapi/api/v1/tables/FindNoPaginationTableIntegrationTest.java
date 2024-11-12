5e9631a8123e2ac7226053210cd95fc438048ccc
Aaron Morton <aaron.morton@datastax.com>
2024-11-13 08:16:32 +1300

    fmt

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/FindNoPaginationTableIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/FindNoPaginationTableIntegrationTest.java
@@ -1,16 +1,14 @@
 package io.stargate.sgv2.jsonapi.api.v1.tables;
 
+import static io.stargate.sgv2.jsonapi.api.v1.util.DataApiCommandSenders.assertTableCommand;
+
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import io.stargate.sgv2.jsonapi.api.v1.util.scenarios.KeyValueTable100Scenario;
 import io.stargate.sgv2.jsonapi.api.v1.util.scenarios.KeyValueTable10Scenario;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
-import org.junit.jupiter.api.*;
-
 import java.util.List;
 import java.util.Map;
-
-import static io.stargate.sgv2.jsonapi.api.v1.util.DataApiCommandSenders.assertTableCommand;
+import org.junit.jupiter.api.*;
 
 @QuarkusIntegrationTest
 @WithTestResource(value = DseTestResource.class, restrictToAnnotatedClass = false)
