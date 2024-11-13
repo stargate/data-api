c95ecfeda48b950f1fdab27dcdb4a30250895bc5
maheshrajamani <99678631+maheshrajamani@users.noreply.github.com>
2024-11-13 11:17:31 -0500

    Fixed as per review comment

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/CommandType.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/CommandType.java
@@ -3,5 +3,6 @@ package io.stargate.sgv2.jsonapi.api.model.command;
 /** Enum class for command types. This is used to categorize commands into DDL and DML. */
 public enum CommandType {
   DDL,
-  DML
+  DML,
+  ADMIN
 }
