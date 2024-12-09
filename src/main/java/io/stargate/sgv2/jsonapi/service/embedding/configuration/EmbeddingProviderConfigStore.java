81984ba925dcaac2f1ae03479d731cec3adfa693
Kathiresan Selvaraj <96088452+kathirsvn@users.noreply.github.com>
2024-12-09 14:34:39 -0500

    Added optional 'service_url_override' in ModelConfig (#1783)

--- src/main/java/io/stargate/sgv2/jsonapi/service/embedding/configuration/EmbeddingProviderConfigStore.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/embedding/configuration/EmbeddingProviderConfigStore.java
@@ -1,5 +1,6 @@
 package io.stargate.sgv2.jsonapi.service.embedding.configuration;
 
+import java.util.Map;
 import java.util.Optional;
 
 public interface EmbeddingProviderConfigStore {
@@ -10,19 +11,31 @@ public interface EmbeddingProviderConfigStore {
       String baseUrl,
       // `implementationClass` is the custom class that implements the EmbeddingProvider interface
       Optional<Class<?>> implementationClass,
-      RequestProperties requestConfiguration) {
+      RequestProperties requestConfiguration,
+      Map<String, Optional<String>> modelUrlOverrides) {
 
     public static ServiceConfig provider(
         String serviceName,
         String serviceProvider,
         String baseUrl,
-        RequestProperties requestConfiguration) {
-      return new ServiceConfig(serviceName, serviceProvider, baseUrl, null, requestConfiguration);
+        RequestProperties requestConfiguration,
+        Map<String, Optional<String>> modelUrlOverrides) {
+      return new ServiceConfig(
+          serviceName, serviceProvider, baseUrl, null, requestConfiguration, modelUrlOverrides);
     }
 
     public static ServiceConfig custom(Optional<Class<?>> implementationClass) {
       return new ServiceConfig(
-          ProviderConstants.CUSTOM, ProviderConstants.CUSTOM, null, implementationClass, null);
+          ProviderConstants.CUSTOM,
+          ProviderConstants.CUSTOM,
+          null,
+          implementationClass,
+          null,
+          Map.of());
+    }
+
+    public String getBaseUrl(String modelName) {
+      return modelUrlOverrides != null ? modelUrlOverrides.get(modelName).orElse(baseUrl) : baseUrl;
     }
   }
 
