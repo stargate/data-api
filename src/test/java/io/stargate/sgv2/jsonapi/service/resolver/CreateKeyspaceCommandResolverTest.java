1d8ca734c1a6f3c9022e691f12323e0487381ef4
Hazel <hazel.he@datastax.com>
2025-01-24 16:46:13 -0800

    fix and update createKeyspace tests

--- src/test/java/io/stargate/sgv2/jsonapi/service/resolver/CreateKeyspaceCommandResolverTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/resolver/CreateKeyspaceCommandResolverTest.java
@@ -1,6 +1,7 @@
 package io.stargate.sgv2.jsonapi.service.resolver;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.junit.QuarkusTest;
@@ -8,11 +9,13 @@ import io.quarkus.test.junit.TestProfile;
 import io.stargate.sgv2.jsonapi.TestConstants;
 import io.stargate.sgv2.jsonapi.api.model.command.CommandContext;
 import io.stargate.sgv2.jsonapi.api.model.command.impl.CreateNamespaceCommand;
+import io.stargate.sgv2.jsonapi.exception.SchemaException;
 import io.stargate.sgv2.jsonapi.service.cqldriver.executor.DatabaseSchemaObject;
 import io.stargate.sgv2.jsonapi.service.operation.Operation;
 import io.stargate.sgv2.jsonapi.service.operation.keyspaces.CreateKeyspaceOperation;
 import io.stargate.sgv2.jsonapi.testresource.NoGlobalResourcesTestProfile;
 import jakarta.inject.Inject;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -26,7 +29,7 @@ class CreateKeyspaceCommandResolverTest {
   CommandContext<DatabaseSchemaObject> commandContext = TestConstants.DATABASE_CONTEXT;
 
   @Nested
-  class ResolveCommand {
+  class CreateKeyspaceSuccess {
 
     @Test
     public void noOptions() throws Exception {
@@ -172,5 +175,156 @@ class CreateKeyspaceCommandResolverTest {
                 assertThat(op.replicationMap()).isEqualTo("{'class': 'NetworkTopologyStrategy'}");
               });
     }
+  }
+
+  @Nested
+  class CreateKeyspaceFailure {
+
+    @Test
+    public void createKeyspaceWithNull() throws Exception {
+      String json =
+          """
+            {
+              "createNamespace": {
+              }
+            }
+            """;
+
+      CreateNamespaceCommand command = objectMapper.readValue(json, CreateNamespaceCommand.class);
+      Throwable throwable = catchThrowable(() -> resolver.resolveCommand(commandContext, command));
+
+      verifySchemaException(
+          throwable,
+          SchemaException.Code.UNSUPPORTED_SCHEMA_NAME,
+          "The command attempted to create a keyspace with a name that is not supported.",
+          "The supported keyspace names must not be empty, more than 48 characters long, or contain non-alphanumeric-underscore characters.",
+          "The command used the unsupported keyspace name: null.");
+    }
+
+    @Test
+    public void createKeyspaceWithEmptyName() throws Exception {
+      String json =
+          """
+              {
+                "createNamespace": {
+                  "name" : ""
+                }
+              }
+              """;
+
+      CreateNamespaceCommand command = objectMapper.readValue(json, CreateNamespaceCommand.class);
+      Throwable throwable = catchThrowable(() -> resolver.resolveCommand(commandContext, command));
+
+      verifySchemaException(
+          throwable,
+          SchemaException.Code.UNSUPPORTED_SCHEMA_NAME,
+          "The command attempted to create a keyspace with a name that is not supported.",
+          "The supported keyspace names must not be empty, more than 48 characters long, or contain non-alphanumeric-underscore characters.",
+          "The command used the unsupported keyspace name: .");
+    }
+
+    @Test
+    public void createKeyspaceWithBlankName() throws Exception {
+      String json =
+          """
+                    {
+                      "createNamespace": {
+                         "name" : " "
+                      }
+                    }
+                    """;
+
+      CreateNamespaceCommand command = objectMapper.readValue(json, CreateNamespaceCommand.class);
+      Throwable throwable = catchThrowable(() -> resolver.resolveCommand(commandContext, command));
+
+      verifySchemaException(
+          throwable,
+          SchemaException.Code.UNSUPPORTED_SCHEMA_NAME,
+          "The command attempted to create a keyspace with a name that is not supported.",
+          "The supported keyspace names must not be empty, more than 48 characters long, or contain non-alphanumeric-underscore characters.",
+          "The command used the unsupported keyspace name:  .");
+    }
+
+    @Test
+    public void createKeyspaceWithNameTooLong() throws Exception {
+      String name = RandomStringUtils.randomAlphabetic(49);
+      String json =
+              """
+            {
+              "createNamespace": {
+                "name": "%s"
+              }
+            }
+            """
+              .formatted(name);
+
+      CreateNamespaceCommand command = objectMapper.readValue(json, CreateNamespaceCommand.class);
+      Throwable throwable = catchThrowable(() -> resolver.resolveCommand(commandContext, command));
+
+      verifySchemaException(
+          throwable,
+          SchemaException.Code.UNSUPPORTED_SCHEMA_NAME,
+          "The command attempted to create a keyspace with a name that is not supported.",
+          "The supported keyspace names must not be empty, more than 48 characters long, or contain non-alphanumeric-underscore characters.",
+          "The command used the unsupported keyspace name: %s.".formatted(name));
+    }
+
+    @Test
+    public void createKeyspaceWithEmptySpace() throws Exception {
+      String json =
+          """
+            {
+              "createNamespace": {
+                "name" : "a b"
+              }
+            }
+            """;
+
+      CreateNamespaceCommand command = objectMapper.readValue(json, CreateNamespaceCommand.class);
+      Throwable throwable = catchThrowable(() -> resolver.resolveCommand(commandContext, command));
+
+      verifySchemaException(
+          throwable,
+          SchemaException.Code.UNSUPPORTED_SCHEMA_NAME,
+          "The command attempted to create a keyspace with a name that is not supported.",
+          "The supported keyspace names must not be empty, more than 48 characters long, or contain non-alphanumeric-underscore characters.",
+          "The command used the unsupported keyspace name: a b.");
+    }
+
+    @Test
+    public void createKeyspaceWithSpecialCharacter() throws Exception {
+      String json =
+          """
+              {
+                "createNamespace": {
+                  "name" : "!@-"
+                }
+              }
+              """;
+
+      CreateNamespaceCommand command = objectMapper.readValue(json, CreateNamespaceCommand.class);
+      Throwable throwable = catchThrowable(() -> resolver.resolveCommand(commandContext, command));
+
+      verifySchemaException(
+          throwable,
+          SchemaException.Code.UNSUPPORTED_SCHEMA_NAME,
+          "The command attempted to create a keyspace with a name that is not supported.",
+          "The supported keyspace names must not be empty, more than 48 characters long, or contain non-alphanumeric-underscore characters.",
+          "The command used the unsupported keyspace name: !@-.");
+    }
+  }
+
+  private void verifySchemaException(
+      Throwable throwable, SchemaException.Code exceptedErrorCode, String... messageSnippet) {
+    assertThat(throwable)
+        .isInstanceOf(SchemaException.class)
+        .satisfies(
+            e -> {
+              SchemaException exception = (SchemaException) e;
+              assertThat(exception.code).isEqualTo(exceptedErrorCode.name());
+              for (String snippet : messageSnippet) {
+                assertThat(exception.getMessage()).contains(snippet);
+              }
+            });
   }
 }
