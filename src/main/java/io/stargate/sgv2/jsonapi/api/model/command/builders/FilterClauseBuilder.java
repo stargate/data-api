738f081014972211aae332b17322f3cfd8a1913e
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-01-29 11:47:41 -0800

    More shuffling

--- src/main/java/io/stargate/sgv2/jsonapi/api/model/command/builders/FilterClauseBuilder.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/model/command/builders/FilterClauseBuilder.java
@@ -67,7 +67,11 @@ public abstract class FilterClauseBuilder<T extends SchemaObject> {
     return buildAndValidate(implicitAnd);
   }
 
+  // // // Abstract methods for sub-classes to implement
+
   protected abstract FilterClause buildAndValidate(LogicalExpression implicitAnd);
+
+  // // // Construction of LogicalExpression from JSON
 
   private void populateExpression(LogicalExpression logicalExpression, JsonNode node) {
     if (logicalExpression == null) {
@@ -143,6 +147,186 @@ public abstract class FilterClauseBuilder<T extends SchemaObject> {
                   entry.getKey(), jsonNodeValue(entry.getKey(), entry.getValue()))));
     }
   }
+
+  /**
+   * The filter clause is entry will have field path as key and object type as value. The value can
+   * have multiple operator and condition values.
+   *
+   * <p>Eg 1: {"field" : {"$eq" : "value"}}
+   *
+   * <p>Eg 2: {"field" : {"$gt" : 10, "$lt" : 50}}
+   *
+   * @param entry
+   * @return
+   */
+  private List<ComparisonExpression> createComparisonExpressionList(
+      Map.Entry<String, JsonNode> entry) {
+    final List<ComparisonExpression> comparisonExpressionList = new ArrayList<>();
+    final Iterator<Map.Entry<String, JsonNode>> fields = entry.getValue().fields();
+    while (fields.hasNext()) {
+      Map.Entry<String, JsonNode> updateField = fields.next();
+      final String updateKey = updateField.getKey();
+      FilterOperator operator =
+          FilterOperator.FilterOperatorUtils.findComparisonOperator(updateKey);
+
+      // If assumed filter not found, may be JSON Extension value like "$date" or "$uuid";
+      // or may be full Object to match
+      if (operator == null) {
+        JsonExtensionType etype = JsonUtil.findJsonExtensionType(updateKey);
+        if ((etype == null) && updateKey.startsWith("$")) {
+          throw ErrorCodeV1.UNSUPPORTED_FILTER_OPERATION.toApiException(updateKey);
+        }
+        if (!DocumentConstants.Fields.VALID_PATH_PATTERN.matcher(entry.getKey()).matches()) {
+          throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
+              "filter clause path ('%s') contains character(s) not allowed", entry.getKey());
+        }
+        // JSON Extension type needs to be explicitly handled:
+        Object value;
+        if (etype != null) {
+          if (entry.getKey().equals(DOC_ID)) {
+            value = DocumentId.fromJson(entry.getValue());
+          } else {
+            value = JsonUtil.extractExtendedValue(etype, updateField);
+          }
+        } else {
+          // Otherwise we have a full JSON Object to match:
+          value = jsonNodeValue(entry.getKey(), entry.getValue());
+        }
+        comparisonExpressionList.add(ComparisonExpression.eq(entry.getKey(), value));
+        return comparisonExpressionList;
+      }
+
+      // if the key does not match pattern or the entry is not ($vector and $exist operator)
+      // combination, throw error
+      if (!(DocumentConstants.Fields.VALID_PATH_PATTERN.matcher(entry.getKey()).matches()
+          || (entry.getKey().equals(DocumentConstants.Fields.VECTOR_EMBEDDING_FIELD)
+              && updateField.getKey().equals("$exists"))
+          || (entry.getKey().equals(DocumentConstants.Fields.VECTOR_EMBEDDING_TEXT_FIELD)
+              && updateField.getKey().equals("$exists")))) {
+        throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
+            "filter clause path ('%s') contains character(s) not allowed", entry.getKey());
+      }
+      JsonNode value = updateField.getValue();
+      Object valueObject = jsonNodeValue(entry.getKey(), value);
+      if (operator == ValueComparisonOperator.GT
+          || operator == ValueComparisonOperator.GTE
+          || operator == ValueComparisonOperator.LT
+          || operator == ValueComparisonOperator.LTE) {
+        // Note, added 'valueObject instanceof String || valueObject instanceof Boolean', this is to
+        // unblock some table filter against non-numeric column
+        // e.g. {"event_date": {"$gt": "2024-09-24"}}, {"is_active": {"$gt": true}},
+        // {"name":{"$gt":"Tim"}}
+        // Also, for collection path, this will allow comparison filter against collection maps
+        // query_bool_values and query_text_values
+        if (!(valueObject instanceof Date
+            || valueObject instanceof String
+            || valueObject instanceof Boolean
+            || valueObject instanceof BigDecimal
+            || (valueObject instanceof DocumentId && (value.isObject() || value.isNumber())))) {
+          throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
+              "%s operator must have `DATE` or `NUMBER` or `TEXT` or `BOOLEAN` value",
+              operator.getOperator());
+        }
+      }
+      ComparisonExpression expression =
+          new ComparisonExpression(entry.getKey(), new ArrayList<>(), null);
+      expression.add(operator, valueObject);
+      comparisonExpressionList.add(expression);
+    }
+    return comparisonExpressionList;
+  }
+
+  /**
+   * Method to parse each filter clause and return node value.
+   *
+   * @param path - If the path is _id, then the value is resolved as DocumentId
+   * @param node - JsonNode which has the operand value of a filter clause
+   * @return
+   */
+  private Object jsonNodeValue(String path, JsonNode node) {
+    // If the path is _id, then the value is resolved as DocumentId and Array type handled for `$in`
+    // operator in filter
+    if (path.equals(DOC_ID)) {
+      if (node.getNodeType() == JsonNodeType.ARRAY) {
+        ArrayNode arrayNode = (ArrayNode) node;
+        List<Object> arrayVals = new ArrayList<>(arrayNode.size());
+        for (JsonNode element : arrayNode) {
+          arrayVals.add(jsonNodeValue(path, element));
+        }
+        return arrayVals;
+      } else {
+        return DocumentId.fromJson(node);
+      }
+    }
+    return jsonNodeValue(node);
+  }
+
+  /**
+   * Method to parse each filter clause and return node value. Called recursively in case of array
+   * and object json types.
+   *
+   * @param node
+   * @return
+   */
+  private Object jsonNodeValue(JsonNode node) {
+    switch (node.getNodeType()) {
+      case BOOLEAN:
+        return node.booleanValue();
+      case NUMBER:
+        return node.decimalValue();
+      case STRING:
+        return node.textValue();
+      case NULL:
+        return null;
+      case ARRAY:
+        {
+          ArrayNode arrayNode = (ArrayNode) node;
+          List<Object> arrayVals = new ArrayList<>(arrayNode.size());
+          for (JsonNode element : arrayNode) {
+            arrayVals.add(jsonNodeValue(element));
+          }
+          return arrayVals;
+        }
+      case OBJECT:
+        {
+          if (JsonUtil.looksLikeEJsonValue(node)) { // means it's a single-entry Map, key
+            JsonExtensionType etype = JsonUtil.findJsonExtensionType(node);
+            if (etype == JsonExtensionType.EJSON_DATE) {
+              JsonNode value = node.iterator().next();
+              if (value.isIntegralNumber() && value.canConvertToLong()) {
+                return new Date(value.longValue());
+              }
+              throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
+                  "$date value has to be sent as epoch time");
+            } else if (etype != null) {
+              // This will convert to Java value if valid value; we'll just convert back to String
+              // since all non-Date JSON extension values are indexed as Constants
+              Object evalue = JsonUtil.extractExtendedValue(etype, node);
+              return evalue.toString();
+            } else {
+              // handle an invalid filter use case:
+              // { "address": { "street": { "$xx": xxx } } }
+              throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
+                  "Invalid use of %s operator", node.fieldNames().next());
+            }
+          } else {
+            ObjectNode objectNode = (ObjectNode) node;
+            Map<String, Object> values = new LinkedHashMap<>(objectNode.size());
+            final Iterator<Map.Entry<String, JsonNode>> fields = objectNode.fields();
+            while (fields.hasNext()) {
+              final Map.Entry<String, JsonNode> nextField = fields.next();
+              values.put(nextField.getKey(), jsonNodeValue(nextField.getValue()));
+            }
+            return values;
+          }
+        }
+      default:
+        throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
+            "Unsupported NodeType %s", node.getNodeType());
+    }
+  }
+
+  // // // Validation of LogicalExpressions before building FilterClause
 
   private void validateExpression(
       OperationsConfig operationsConfig, LogicalExpression logicalExpression) {
@@ -247,184 +431,6 @@ public abstract class FilterClauseBuilder<T extends SchemaObject> {
           }
           break;
       }
-    }
-  }
-
-  /**
-   * The filter clause is entry will have field path as key and object type as value. The value can
-   * have multiple operator and condition values.
-   *
-   * <p>Eg 1: {"field" : {"$eq" : "value"}}
-   *
-   * <p>Eg 2: {"field" : {"$gt" : 10, "$lt" : 50}}
-   *
-   * @param entry
-   * @return
-   */
-  private List<ComparisonExpression> createComparisonExpressionList(
-      Map.Entry<String, JsonNode> entry) {
-    final List<ComparisonExpression> comparisonExpressionList = new ArrayList<>();
-    final Iterator<Map.Entry<String, JsonNode>> fields = entry.getValue().fields();
-    while (fields.hasNext()) {
-      Map.Entry<String, JsonNode> updateField = fields.next();
-      final String updateKey = updateField.getKey();
-      FilterOperator operator =
-          FilterOperator.FilterOperatorUtils.findComparisonOperator(updateKey);
-
-      // If assumed filter not found, may be JSON Extension value like "$date" or "$uuid";
-      // or may be full Object to match
-      if (operator == null) {
-        JsonExtensionType etype = JsonUtil.findJsonExtensionType(updateKey);
-        if ((etype == null) && updateKey.startsWith("$")) {
-          throw ErrorCodeV1.UNSUPPORTED_FILTER_OPERATION.toApiException(updateKey);
-        }
-        if (!DocumentConstants.Fields.VALID_PATH_PATTERN.matcher(entry.getKey()).matches()) {
-          throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
-              "filter clause path ('%s') contains character(s) not allowed", entry.getKey());
-        }
-        // JSON Extension type needs to be explicitly handled:
-        Object value;
-        if (etype != null) {
-          if (entry.getKey().equals(DOC_ID)) {
-            value = DocumentId.fromJson(entry.getValue());
-          } else {
-            value = JsonUtil.extractExtendedValue(etype, updateField);
-          }
-        } else {
-          // Otherwise we have a full JSON Object to match:
-          value = jsonNodeValue(entry.getKey(), entry.getValue());
-        }
-        comparisonExpressionList.add(ComparisonExpression.eq(entry.getKey(), value));
-        return comparisonExpressionList;
-      }
-
-      // if the key does not match pattern or the entry is not ($vector and $exist operator)
-      // combination, throw error
-      if (!(DocumentConstants.Fields.VALID_PATH_PATTERN.matcher(entry.getKey()).matches()
-          || (entry.getKey().equals(DocumentConstants.Fields.VECTOR_EMBEDDING_FIELD)
-              && updateField.getKey().equals("$exists"))
-          || (entry.getKey().equals(DocumentConstants.Fields.VECTOR_EMBEDDING_TEXT_FIELD)
-              && updateField.getKey().equals("$exists")))) {
-        throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
-            "filter clause path ('%s') contains character(s) not allowed", entry.getKey());
-      }
-      JsonNode value = updateField.getValue();
-      Object valueObject = jsonNodeValue(entry.getKey(), value);
-      if (operator == ValueComparisonOperator.GT
-          || operator == ValueComparisonOperator.GTE
-          || operator == ValueComparisonOperator.LT
-          || operator == ValueComparisonOperator.LTE) {
-        // Note, added 'valueObject instanceof String || valueObject instanceof Boolean', this is to
-        // unblock some table filter against non-numeric column
-        // e.g. {"event_date": {"$gt": "2024-09-24"}}, {"is_active": {"$gt": true}},
-        // {"name":{"$gt":"Tim"}}
-        // Also, for collection path, this will allow comparison filter against collection maps
-        // query_bool_values and query_text_values
-        if (!(valueObject instanceof Date
-            || valueObject instanceof String
-            || valueObject instanceof Boolean
-            || valueObject instanceof BigDecimal
-            || (valueObject instanceof DocumentId && (value.isObject() || value.isNumber())))) {
-          throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
-              "%s operator must have `DATE` or `NUMBER` or `TEXT` or `BOOLEAN` value",
-              operator.getOperator());
-        }
-      }
-      ComparisonExpression expression =
-          new ComparisonExpression(entry.getKey(), new ArrayList<>(), null);
-      expression.add(operator, valueObject);
-      comparisonExpressionList.add(expression);
-    }
-    return comparisonExpressionList;
-  }
-
-  /**
-   * Method to parse each filter clause and return node value.
-   *
-   * @param path - If the path is _id, then the value is resolved as DocumentId
-   * @param node - JsonNode which has the operand value of a filter clause
-   * @return
-   */
-  private static Object jsonNodeValue(String path, JsonNode node) {
-    // If the path is _id, then the value is resolved as DocumentId and Array type handled for `$in`
-    // operator in filter
-    if (path.equals(DOC_ID)) {
-      if (node.getNodeType() == JsonNodeType.ARRAY) {
-        ArrayNode arrayNode = (ArrayNode) node;
-        List<Object> arrayVals = new ArrayList<>(arrayNode.size());
-        for (JsonNode element : arrayNode) {
-          arrayVals.add(jsonNodeValue(path, element));
-        }
-        return arrayVals;
-      } else {
-        return DocumentId.fromJson(node);
-      }
-    }
-    return jsonNodeValue(node);
-  }
-
-  /**
-   * Method to parse each filter clause and return node value. Called recursively in case of array
-   * and object json types.
-   *
-   * @param node
-   * @return
-   */
-  private static Object jsonNodeValue(JsonNode node) {
-    switch (node.getNodeType()) {
-      case BOOLEAN:
-        return node.booleanValue();
-      case NUMBER:
-        return node.decimalValue();
-      case STRING:
-        return node.textValue();
-      case NULL:
-        return null;
-      case ARRAY:
-        {
-          ArrayNode arrayNode = (ArrayNode) node;
-          List<Object> arrayVals = new ArrayList<>(arrayNode.size());
-          for (JsonNode element : arrayNode) {
-            arrayVals.add(jsonNodeValue(element));
-          }
-          return arrayVals;
-        }
-      case OBJECT:
-        {
-          if (JsonUtil.looksLikeEJsonValue(node)) { // means it's a single-entry Map, key
-            JsonExtensionType etype = JsonUtil.findJsonExtensionType(node);
-            if (etype == JsonExtensionType.EJSON_DATE) {
-              JsonNode value = node.iterator().next();
-              if (value.isIntegralNumber() && value.canConvertToLong()) {
-                return new Date(value.longValue());
-              }
-              throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
-                  "$date value has to be sent as epoch time");
-            } else if (etype != null) {
-              // This will convert to Java value if valid value; we'll just convert back to String
-              // since all non-Date JSON extension values are indexed as Constants
-              Object evalue = JsonUtil.extractExtendedValue(etype, node);
-              return evalue.toString();
-            } else {
-              // handle an invalid filter use case:
-              // { "address": { "street": { "$xx": xxx } } }
-              throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
-                  "Invalid use of %s operator", node.fieldNames().next());
-            }
-          } else {
-            ObjectNode objectNode = (ObjectNode) node;
-            Map<String, Object> values = new LinkedHashMap<>(objectNode.size());
-            final Iterator<Map.Entry<String, JsonNode>> fields = objectNode.fields();
-            while (fields.hasNext()) {
-              final Map.Entry<String, JsonNode> nextField = fields.next();
-              values.put(nextField.getKey(), jsonNodeValue(nextField.getValue()));
-            }
-            return values;
-          }
-        }
-      default:
-        throw ErrorCodeV1.INVALID_FILTER_EXPRESSION.toApiException(
-            "Unsupported NodeType %s", node.getNodeType());
     }
   }
 }
