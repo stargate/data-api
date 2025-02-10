d062922e3155415269539c5e399ce117b4937afb
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-02-10 08:44:56 -0800

    Javadoc/comment improvements

--- src/main/java/io/stargate/sgv2/jsonapi/service/schema/naming/FieldNamingRule.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/schema/naming/FieldNamingRule.java
@@ -1,5 +1,18 @@
 package io.stargate.sgv2.jsonapi.service.schema.naming;
 
+/**
+ * {@link NamingRule} for validating field names: names that come from properties of JSON document
+ * used to create a Document in a Collection. These names form a hierarchy of fields, where each
+ * field can have sub-fields.
+ *
+ * <p>The rules for field names are:
+ *
+ * <ul>
+ *   <li>Field names must not start with a dollar sign ($).
+ *   <li>Field names are hierarchical (forming a Path), but validation is done on name-by-name
+ *       basis.
+ * </ul>
+ */
 public class FieldNamingRule extends NamingRule {
   public FieldNamingRule() {
     super("Field");
@@ -7,14 +20,14 @@ public class FieldNamingRule extends NamingRule {
 
   /**
    * Validates the given Field name. The only limit is that the name must NOT start with a dollar
-   * sign ($). Note that field names are hierarchical, but validation is done on per-segment basis.
+   * sign ($). Note that field names are hierarchical, but validation is done on name-by-name basis.
    *
    * @param depth Level of the field in the hierarchy (1 for root, 2 for first child, etc.)
    * @param name the name to validate
    * @return true if the name is valid, false otherwise
    */
   public boolean apply(int depth, String name) {
-    // Dollar not allowed for any fields (not just root)
+    // Dollar not allowed to start any field name (not just root)
     return !name.startsWith("$");
   }
 }
