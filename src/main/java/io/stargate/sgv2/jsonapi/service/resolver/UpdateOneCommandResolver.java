add23d372c433a3fe5d31624fb0f309a6f3c8415
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-03-21 09:13:38 -0700

    Fix #1939: support sort on $lexical for "findOneAndXxx", "deleteOne", "updateOne" (#1942)

--- src/main/java/io/stargate/sgv2/jsonapi/service/resolver/UpdateOneCommandResolver.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/resolver/UpdateOneCommandResolver.java
@@ -10,7 +10,6 @@ import io.stargate.sgv2.jsonapi.api.model.command.clause.sort.SortExpression;
 import io.stargate.sgv2.jsonapi.api.model.command.impl.UpdateOneCommand;
 import io.stargate.sgv2.jsonapi.api.v1.metrics.JsonApiMetricsConfig;
 import io.stargate.sgv2.jsonapi.config.OperationsConfig;
-import io.stargate.sgv2.jsonapi.exception.ErrorCodeV1;
 import io.stargate.sgv2.jsonapi.exception.SortException;
 import io.stargate.sgv2.jsonapi.service.cqldriver.executor.TableSchemaObject;
 import io.stargate.sgv2.jsonapi.service.embedding.DataVectorizerService;
@@ -171,18 +170,13 @@ public class UpdateOneCommandResolver implements CommandResolver<UpdateOneComman
     // BM25 search / sort?
     SortExpression bm25Expr = SortClauseUtil.resolveBM25Search(sortClause);
     if (bm25Expr != null) {
-      throw ErrorCodeV1.INVALID_SORT_CLAUSE.toApiException(
-          "BM25 search is not yet supported for this command");
-      // Likely implementation of [data-api#1939] to support BM25 sort
-      /*
       return FindCollectionOperation.bm25Single(
-              commandContext,
-              dbLogicalExpression,
-              DocumentProjector.includeAllProjector(),
-              CollectionReadType.DOCUMENT,
-              objectMapper,
-              bm25Expr);
-       */
+          commandContext,
+          dbLogicalExpression,
+          DocumentProjector.includeAllProjector(),
+          CollectionReadType.DOCUMENT,
+          objectMapper,
+          bm25Expr);
     }
 
     List<FindCollectionOperation.OrderBy> orderBy = SortClauseUtil.resolveOrderBy(sortClause);
--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/FindCollectionWithLexicalSortIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/FindCollectionWithLexicalSortIntegrationTest.java
@@ -1,7 +1,9 @@
 package io.stargate.sgv2.jsonapi.api.v1;
 
 import static io.stargate.sgv2.jsonapi.api.v1.ResponseAssertions.responseIsFindSuccess;
+import static io.stargate.sgv2.jsonapi.api.v1.ResponseAssertions.responseIsStatusOnly;
 import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -9,6 +11,7 @@ import static org.hamcrest.Matchers.is;
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
+import java.util.Map;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.Nested;
@@ -17,6 +20,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
+/**
+ * Tests for the Lexical sort feature in the JSON API, with the following commands:
+ *
+ * <ul>
+ *   <li>"find" and "findOne"
+ *   <li>"findOneAndUpdate", "findOneAndReplace", "findOneAndDelete"
+ *   <li>"updateOne" and "deleteOne"
+ * </ul>
+ */
 @QuarkusIntegrationTest
 @WithTestResource(value = DseTestResource.class, restrictToAnnotatedClass = false)
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
@@ -28,11 +40,11 @@ public class FindCollectionWithLexicalSortIntegrationTest
   static final String COLLECTION_WITHOUT_LEXICAL =
       "coll_no_lexical_sort_" + RandomStringUtils.randomNumeric(16);
 
-  static final String DOC1_JSON = lexicalDoc(1, "monkey banana");
-  static final String DOC2_JSON = lexicalDoc(2, "monkey");
-  static final String DOC3_JSON = lexicalDoc(3, "biking fun");
-  static final String DOC4_JSON = lexicalDoc(4, "banana");
-  static final String DOC5_JSON = lexicalDoc(5, "fun");
+  static final String DOC1_JSON = lexicalDoc(1, "monkey banana", "value1");
+  static final String DOC2_JSON = lexicalDoc(2, "monkey", "value2");
+  static final String DOC3_JSON = lexicalDoc(3, "biking fun", "value3");
+  static final String DOC4_JSON = lexicalDoc(4, "banana bread with butter", "value4");
+  static final String DOC5_JSON = lexicalDoc(5, "fun", "value5");
 
   @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
   @Nested
@@ -98,8 +110,8 @@ public class FindCollectionWithLexicalSortIntegrationTest
                           """)
           .body("$", responseIsFindSuccess())
           .body("data.documents", hasSize(2))
-          .body("data.documents[0]._id", is("lexical-4"))
-          .body("data.documents[1]._id", is("lexical-1"));
+          .body("data.documents[0]._id", is("lexical-1"))
+          .body("data.documents[1]._id", is("lexical-4"));
     }
   }
 
@@ -147,7 +159,7 @@ public class FindCollectionWithLexicalSortIntegrationTest
   @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
   @Nested
   @Order(3)
-  class FailingCases {
+  class FailingCasesFindMany {
     @Test
     void failIfLexicalDisabledForCollection() {
       givenHeadersPostJsonThenOk(
@@ -207,14 +219,217 @@ public class FindCollectionWithLexicalSortIntegrationTest
     }
   }
 
-  static String lexicalDoc(int id, String keywords) {
+  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
+  @Nested
+  @Order(10)
+  class HappyCasesFindOneAndUpdate {
+    @Test
+    void findOneAndUpdateWithSort() {
+      final String expectedAfterChange = lexicalDoc(1, "monkey banana", "value1-updated");
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+           {
+             "findOneAndUpdate": {
+               "sort": { "$lexical": "banana" },
+               "update" : {"$set" : {"value": "value1-updated"}},
+               "projection": {"$lexical": 1 },
+               "options": {"returnDocument": "after"}
+             }
+           }
+           """)
+          .body("data.document", jsonEquals(expectedAfterChange))
+          .body("status.matchedCount", is(1))
+          .body("status.modifiedCount", is(1));
+      // Plus query to check that the document was updated
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+          {
+            "findOne": {
+              "filter" : {"_id" : "lexical-1"},
+              "projection": {"*": 1 }
+            }
+          }
+          """)
+          .body("$", responseIsFindSuccess())
+          .body("data.document", jsonEquals(expectedAfterChange));
+    }
+  }
+
+  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
+  @Nested
+  @Order(11)
+  class HappyCasesUpdateOne {
+    @Test
+    void updateOneWithSort() {
+      final String expectedAfterChange = lexicalDoc(1, "monkey banana", "value1-updated-2");
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+           {
+             "updateOne": {
+               "sort": { "$lexical": "banana" },
+               "update" : {"$set" : {"value": "value1-updated-2"}}
+             }
+           }
+           """)
+          .body("status.matchedCount", is(1))
+          .body("status.modifiedCount", is(1));
+      // Plus query to check that the document was updated
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+          {
+            "findOne": {
+              "filter" : {"_id" : "lexical-1"},
+              "projection": {"*": 1 }
+            }
+          }
+          """)
+          .body("$", responseIsFindSuccess())
+          .body("data.document", jsonEquals(expectedAfterChange));
+    }
+  }
+
+  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
+  @Nested
+  @Order(12)
+  class HappyCasesFindOneAndReplace {
+    @Test
+    void findOneAndReplaceWithSort() {
+      final String expectedAfterChange = lexicalDoc(1, "monkey banana", "value1-replaced");
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+                  """
+           {
+             "findOneAndReplace": {
+               "sort": { "$lexical": "banana" },
+               "replacement" : %s,
+               "projection": {"$lexical": 1 },
+               "options": {"returnDocument": "after"}
+             }
+           }
+           """
+                  .formatted(expectedAfterChange))
+          .body("data.document", jsonEquals(expectedAfterChange))
+          .body("status.matchedCount", is(1))
+          .body("status.modifiedCount", is(1));
+      // Plus query to check that the document was updated
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+          {
+            "findOne": {
+              "filter" : {"_id" : "lexical-1"},
+              "projection": {"*": 1 }
+            }
+          }
+          """)
+          .body("$", responseIsFindSuccess())
+          .body("data.document", jsonEquals(expectedAfterChange));
+    }
+  }
+
+  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
+  @Nested
+  @Order(13)
+  class HappyCasesFindOneAndDelete {
+    @Test
+    void findOneAndDeleteWithSort() {
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+                      {
+                        "findOneAndDelete": {
+                          "sort": { "$lexical": "monkey" },
+                          "projection": {"$lexical": 1 }
+                        }
+                      }
+                      """)
+          .body("status.deletedCount", is(1))
+          .body("data.document", jsonEquals(DOC2_JSON));
+
+      // Since "lexical-2" deleted, should only have 4 documents left
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+          {
+            "find": {
+              "projection": {"_id": 1, "value": 0 }
+            }
+          }
+          """)
+          .body("$", responseIsFindSuccess())
+          .body(
+              "data.documents",
+              containsInAnyOrder(
+                  Map.of("_id", "lexical-1"),
+                  Map.of("_id", "lexical-3"),
+                  Map.of("_id", "lexical-4"),
+                  Map.of("_id", "lexical-5")));
+    }
+  }
+
+  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
+  @Nested
+  @Order(14)
+  class HappyCasesDeleteOne {
+    @Test
+    void deleteOneWithSort() {
+      // delete doc "lexical-3":
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+                      {
+                        "deleteOne": {
+                          "filter": { },
+                          "sort": { "$lexical": "biking" }
+                        }
+                      }
+                      """)
+          .body("$", responseIsStatusOnly())
+          .body("status.deletedCount", is(1));
+
+      // Should now delete "lexical-1", leaving 3 documents
+      givenHeadersPostJsonThenOkNoErrors(
+              keyspaceName,
+              COLLECTION_WITH_LEXICAL,
+              """
+          {
+            "find": {
+              "projection": {"_id": 1, "value": 0 }
+            }
+          }
+          """)
+          .body("$", responseIsFindSuccess())
+          .body(
+              "data.documents",
+              containsInAnyOrder(
+                  Map.of("_id", "lexical-1"),
+                  Map.of("_id", "lexical-4"),
+                  Map.of("_id", "lexical-5")));
+    }
+  }
+
+  static String lexicalDoc(int id, String keywords, String value) {
     return
         """
             {
               "_id": "lexical-%d",
-              "$lexical": "%s"
+              "$lexical": "%s",
+              "value": "%s"
             }
         """
-        .formatted(id, keywords);
+        .formatted(id, keywords, value);
   }
 }
