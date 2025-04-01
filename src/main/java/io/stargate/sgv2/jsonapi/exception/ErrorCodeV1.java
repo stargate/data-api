d5da65ce7cbcb12b6e9389fb7a0ca64620685998
Yuqi Du <istimdu@gmail.com>
2025-04-01 15:14:20 -0700

    fix the colon i ErrorCodeV1

--- src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
+++ src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
@@ -96,7 +96,7 @@ public enum ErrorCodeV1 {
   INVALID_JSONAPI_COLLECTION_SCHEMA("Not a valid json api collection schema"),
 
   RERANKING_FEATURE_NOT_ENABLED("Reranking feature is not enabled"),
-  RERANKING_SERVICE_TYPE_UNAVAILABLE("Reranking service unavailable : "),
+  RERANKING_SERVICE_TYPE_UNAVAILABLE("Reranking service unavailable"),
   RERANKING_PROVIDER_UNEXPECTED_RESPONSE("The Reranking Provider returned an unexpected response"),
   RERANKING_PROVIDER_CLIENT_ERROR("The Reranking Provider returned a HTTP client error"),
   RERANKING_PROVIDER_SERVER_ERROR("The Reranking Provider returned a HTTP server error"),
