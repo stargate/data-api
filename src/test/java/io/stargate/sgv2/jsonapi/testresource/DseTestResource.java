9e88c5b2410359a9444d1b14986f1fb8ec20a492
Yuqi Du <istimdu@gmail.com>
2025-04-02 13:10:31 -0700

    fix attempt for test configuration file

--- src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
+++ src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
@@ -100,8 +100,7 @@ public class DseTestResource extends StargateTestResource {
 
     // Integration tests load provider yaml files from test resources folder.
     // This is to help with testing non-prod provider configuration. E.G. model deprecation.
-    propsBuilder.put(
-        "RERANKING_CONFIG_PATH", "src/test/resources/test-reranking-providers-config.yaml");
+    propsBuilder.put("RERANKING_CONFIG_PATH", "test-reranking-providers-config.yaml");
 
     propsBuilder.put(
         "stargate.jsonapi.custom.embedding.clazz",
