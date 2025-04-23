abf8d266d447398872095df8ccce20b1ef359db4
Hazel <hazel.he@datastax.com>
2025-04-23 09:26:59 -0700

    change back to "user_agent"

--- src/main/java/io/stargate/sgv2/jsonapi/api/v1/metrics/MetricsConfig.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/v1/metrics/MetricsConfig.java
@@ -57,7 +57,7 @@ public interface MetricsConfig {
      * @return The tag key for user-agent flag, defaults to <code>user_agent</code>.
      */
     @NotBlank
-    @WithDefault("user.agent")
+    @WithDefault("user_agent")
     String userAgentTag();
 
     /**
