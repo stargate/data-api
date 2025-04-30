0668ae51a2e9619c555588396f75c2010336e222
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-04-30 10:43:21 -0700

    Fixes #2057: findAndRerank, NPE for empty object

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/FindAndRerankCollectionIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/FindAndRerankCollectionIntegrationTest.java
@@ -1,14 +1,11 @@
 package io.stargate.sgv2.jsonapi.api.v1;
 
-import static io.restassured.RestAssured.given;
-import static io.stargate.sgv2.jsonapi.api.v1.ResponseAssertions.responseIsDDLSuccess;
 import static io.stargate.sgv2.jsonapi.api.v1.ResponseAssertions.responseIsError;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import io.restassured.http.ContentType;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,7 +20,7 @@ import org.junit.jupiter.api.Test;
 @WithTestResource(value = DseTestResource.class, restrictToAnnotatedClass = false)
 public class FindAndRerankCollectionIntegrationTest extends AbstractCollectionIntegrationTestBase {
 
-  // used to cleanup the collection from a previous test, if non null
+  // used to cleanup the collection from a previous test, if non-null
   private String cleanupCollectionName = null;
 
   @BeforeAll
@@ -34,54 +31,10 @@ public class FindAndRerankCollectionIntegrationTest extends AbstractCollectionIn
   @AfterEach
   public void cleanup() {
     if (cleanupCollectionName != null) {
-      var json =
-              """
-          {"dropCollection": {"name": "%s"}}
-          """
-              .formatted(cleanupCollectionName);
-      given()
-          .port(getTestPort())
-          .headers(getHeaders())
-          .contentType(ContentType.JSON)
-          .body(json)
-          .when()
-          .post(KeyspaceResource.BASE_PATH, keyspaceName)
-          .then()
-          .statusCode(200)
-          .body("$", responseIsDDLSuccess());
+      String toDelete = cleanupCollectionName;
+      cleanupCollectionName = null; // clear out just in case
+      deleteCollection(toDelete);
     }
-  }
-
-  public void errorOnNotEnabled(
-      String collectionName, String collectionSpec, String errorCode, String errorMessageContains) {
-
-    var createCollection = collectionSpec.formatted(collectionName);
-    createComplexCollection(createCollection);
-
-    var rerank =
-        """
-      {"findAndRerank": {
-              "filter": {},
-              "projection": {},
-              "sort": {
-                  "$hybrid": "hybrid sort"
-              },
-              "options": {
-                  "limit" : 10,
-                  "hybridLimits" : 10,
-                  "includeScores": true,
-                  "includeSortVector": false
-              }
-          }
-      }
-      """;
-
-    givenHeadersPostJsonThen(keyspaceName, collectionName, rerank)
-        .body("$", responseIsError())
-        .body("errors[0].errorCode", is(errorCode))
-        .body(
-            "errors[0].message",
-            containsString(errorMessageContains.formatted(keyspaceName, collectionName)));
   }
 
   @Test
@@ -141,5 +94,38 @@ public class FindAndRerankCollectionIntegrationTest extends AbstractCollectionIn
         """,
         "LEXICAL_NOT_ENABLED_FOR_COLLECTION",
         "Lexical search is not enabled for collection");
+  }
+
+  private void errorOnNotEnabled(
+      String collectionName, String collectionSpec, String errorCode, String errorMessageContains) {
+
+    var createCollection = collectionSpec.formatted(collectionName);
+    cleanupCollectionName = collectionName;
+    createComplexCollection(createCollection);
+
+    var rerank =
+        """
+          {"findAndRerank": {
+                  "filter": {},
+                  "projection": {},
+                  "sort": {
+                      "$hybrid": "hybrid sort"
+                  },
+                  "options": {
+                      "limit" : 10,
+                      "hybridLimits" : 10,
+                      "includeScores": true,
+                      "includeSortVector": false
+                  }
+              }
+          }
+          """;
+
+    givenHeadersPostJsonThen(keyspaceName, collectionName, rerank)
+        .body("$", responseIsError())
+        .body("errors[0].errorCode", is(errorCode))
+        .body(
+            "errors[0].message",
+            containsString(errorMessageContains.formatted(keyspaceName, collectionName)));
   }
 }
