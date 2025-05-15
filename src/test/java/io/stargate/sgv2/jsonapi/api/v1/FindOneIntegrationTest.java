62d6fce61e53565e677ce46a473cff183862759c
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-05-15 08:28:40 -0700

    Fix an existing IT to test right thing

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/FindOneIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/FindOneIntegrationTest.java
@@ -837,7 +837,7 @@ public class FindOneIntegrationTest extends AbstractCollectionIntegrationTestBas
     public void failForFilteringOnLexical() {
       for (String filter :
           new String[] {
-            "{\"$lexical\": \"quick brown fox\"}", "{\"$lexical\": {\"eq\": \"quick brown fox\"}}"
+            "{\"$lexical\": \"quick brown fox\"}", "{\"$lexical\": {\"$eq\": \"quick brown fox\"}}"
           }) {
         givenHeadersPostJsonThenOk("{ \"findOne\": { \"filter\" : %s}}".formatted(filter))
             .body("$", responseIsError())
