3bafe76cc1aa3d657635d3b7e1ccc36dd8fa0d29
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-05-21 10:46:59 -0700

    Convert 2 small Collection ITs

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/CreateCollectionTooManyTablesIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/CreateCollectionTooManyTablesIntegrationTest.java
@@ -1,13 +1,11 @@
 package io.stargate.sgv2.jsonapi.api.v1;
 
-import static io.restassured.RestAssured.given;
 import static io.stargate.sgv2.jsonapi.api.v1.ResponseAssertions.responseIsDDLSuccess;
 import static io.stargate.sgv2.jsonapi.api.v1.ResponseAssertions.responseIsError;
 import static org.hamcrest.Matchers.is;
 
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import io.restassured.http.ContentType;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.Test;
@@ -58,10 +56,7 @@ class CreateCollectionTooManyTablesIntegrationTest extends AbstractKeyspaceInteg
     // First create maximum number of collections
     for (int i = 1; i <= COLLECTIONS_TO_CREATE; ++i) {
       String json = createTemplate.formatted(i);
-      given()
-          .headers(getHeaders())
-          .contentType(ContentType.JSON)
-          .body(json)
+      givenHeadersAndJson(json)
           .when()
           .post(KeyspaceResource.BASE_PATH, NS)
           .then()
@@ -71,10 +66,7 @@ class CreateCollectionTooManyTablesIntegrationTest extends AbstractKeyspaceInteg
     }
     // And then failure
     String json = createTemplate.formatted(99);
-    given()
-        .headers(getHeaders())
-        .contentType(ContentType.JSON)
-        .body(json)
+    givenHeadersAndJson(json)
         .when()
         .post(KeyspaceResource.BASE_PATH, NS)
         .then()
@@ -92,10 +84,7 @@ class CreateCollectionTooManyTablesIntegrationTest extends AbstractKeyspaceInteg
 
     // But then verify that re-creating an existing one should still succeed
     // (if using same settings)
-    given()
-        .headers(getHeaders())
-        .contentType(ContentType.JSON)
-        .body(createTemplate.formatted(1))
+    givenHeadersAndJson(createTemplate.formatted(1))
         .when()
         .post(KeyspaceResource.BASE_PATH, NS)
         .then()
