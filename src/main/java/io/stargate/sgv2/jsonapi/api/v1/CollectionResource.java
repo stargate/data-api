6e631f8e8d2a8853ae271ead6e275eaee7010e93
Hazel <hazel.he@datastax.com>
2025-05-28 18:36:38 -0700

    clean up

--- src/main/java/io/stargate/sgv2/jsonapi/api/v1/CollectionResource.java
+++ src/main/java/io/stargate/sgv2/jsonapi/api/v1/CollectionResource.java
@@ -247,19 +247,6 @@ public class CollectionResource {
                           .getFirstVectorColumnWithVectorizeDefinition()
                           .orElse(null);
                 }
-                //                EmbeddingProvider embeddingProvider =
-                //                    (vectorColDef == null || vectorColDef.vectorizeDefinition() ==
-                // null)
-                //                        ? null
-                //                        : embeddingProviderFactory.getConfiguration(
-                //                            requestContext.getTenantId(),
-                //                            requestContext.getCassandraToken(),
-                //                            vectorColDef.vectorizeDefinition().provider(),
-                //                            vectorColDef.vectorizeDefinition().modelName(),
-                //                            vectorColDef.vectorSize(),
-                //                            vectorColDef.vectorizeDefinition().parameters(),
-                //                            vectorColDef.vectorizeDefinition().authentication(),
-                //                            command.getClass().getSimpleName());
 
                 EmbeddingProvider embeddingProvider = null;
 
