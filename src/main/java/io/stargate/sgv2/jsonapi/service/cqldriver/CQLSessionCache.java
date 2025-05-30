bb5c2e7429bf016f3237e05a6cd00581dc049729
janehe <siyaoh4@uci.edu>
2025-05-29 18:29:08 -0700

    comment out session.close

--- src/main/java/io/stargate/sgv2/jsonapi/service/cqldriver/CQLSessionCache.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/cqldriver/CQLSessionCache.java
@@ -251,7 +251,7 @@ public class CQLSessionCache {
       // This will be running on a cache tread, any error will not make it to the user
       // So we log it and swallow
       try {
-        sessionHolder.session.close();
+        //        sessionHolder.session.close();
       } catch (Exception e) {
         LOGGER.error("Error closing CQLSession sessionHolder={}", sessionHolder, e);
       }
