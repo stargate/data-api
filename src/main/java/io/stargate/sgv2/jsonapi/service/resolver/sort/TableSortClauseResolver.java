1c48de9c92ed2c9c7404ac2402f2d8de95b1481e
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-07-11 14:49:50 -0700

    Remove unnecessary duplicate validation

--- src/main/java/io/stargate/sgv2/jsonapi/service/resolver/sort/TableSortClauseResolver.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/resolver/sort/TableSortClauseResolver.java
@@ -27,35 +27,4 @@ public abstract class TableSortClauseResolver<
   protected TableSortClauseResolver(OperationsConfig operationsConfig) {
     super(operationsConfig);
   }
-
-  /**
-   * Checks that all the sort columns are known columns in the table.
-   *
-   * @param tableSchemaObject The table schema object to check
-   * @param sortColumns The sort columns to check
-   * @throws {@link SortException} of {@link SortException.Code#CANNOT_SORT_UNKNOWN_COLUMNS} if any
-   *     are not present
-   */
-  protected void checkUnknownSortColumns(
-      TableSchemaObject tableSchemaObject, List<CqlIdentifier> sortColumns) {
-
-    var unknownColumns =
-        sortColumns.stream()
-            .filter(
-                sortIdentifier ->
-                    !tableSchemaObject.apiTableDef().allColumns().containsKey(sortIdentifier))
-            .sorted(CQL_IDENTIFIER_COMPARATOR)
-            .toList();
-
-    if (!unknownColumns.isEmpty()) {
-      throw SortException.Code.CANNOT_SORT_UNKNOWN_COLUMNS.get(
-          errVars(
-              tableSchemaObject,
-              map -> {
-                map.put(
-                    "allColumns", errFmtApiColumnDef(tableSchemaObject.apiTableDef().allColumns()));
-                map.put("unknownColumns", errFmtCqlIdentifier(unknownColumns));
-              }));
-    }
-  }
 }
