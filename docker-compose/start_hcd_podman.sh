17f3a6e48be2a541f0fe2d2a92bff409a3b79c16
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-08-18 14:27:44 -0700

    Improvements from code review

--- docker-compose/start_hcd_podman.sh
+++ docker-compose/start_hcd_podman.sh
@@ -26,11 +26,9 @@ LOGLEVEL=INFO
 DATAAPITAG="v1"
 DATAAPIIMAGE="stargateio/data-api"
 
-# These should come from .env file, not from here
-#HCDTAG="1.2.1-early-preview"
-#HCDIMAGE="cr.dtsx.io/datastax/hcd"
-#HCDIMAGE="559669398656.dkr.ecr.us-west-2.amazonaws.com/engops-shared/hcd/staging/hcd"
-#HCDIMAGE="559669398656.dkr.ecr.us-west-2.amazonaws.com/engops-shared/hcd/prod/hcd"
+# 18-Aug-2025, tatu:  These should come from .env file, not included here
+#HCDTAG=
+#HCDIMAGE=
 HCDONLY="false"
 HCDNODES=1
 
