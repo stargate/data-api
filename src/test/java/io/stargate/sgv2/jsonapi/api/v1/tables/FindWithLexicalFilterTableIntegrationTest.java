17693efb0c7ae6c11a2003ff8725b4b3a7a36057
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-08-22 10:58:24 -0700

    Change suggested by code review

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/FindWithLexicalFilterTableIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/FindWithLexicalFilterTableIntegrationTest.java
@@ -1,5 +1,6 @@
 package io.stargate.sgv2.jsonapi.api.v1.tables;
 
+import static io.stargate.sgv2.jsonapi.api.v1.AbstractKeyspaceIntegrationTestBase.TEST_PROP_LEXICAL_DISABLED;
 import static io.stargate.sgv2.jsonapi.api.v1.util.DataApiCommandSenders.assertNamespaceCommand;
 import static io.stargate.sgv2.jsonapi.api.v1.util.DataApiCommandSenders.assertTableCommand;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -16,32 +17,29 @@ import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 @QuarkusIntegrationTest
 @WithTestResource(value = DseTestResource.class)
+@DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
 public class FindWithLexicalFilterTableIntegrationTest extends AbstractTableIntegrationTestBase {
   private static final String TABLE_NAME = "lexicalFilterTableTest";
 
-  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
-  @Nested
-  @Order(1)
-  class Setup {
-    @Test
-    void createAndPopulateTableWithLexicalIndex() {
-      // Create a table with a lexical index on the "tags" column
-      assertNamespaceCommand(keyspaceName)
-          .templated()
-          .createTable(
-              TABLE_NAME,
-              Map.ofEntries(
-                  Map.entry("id", Map.of("type", "text")),
-                  Map.entry("value", Map.of("type", "text")),
-                  Map.entry("tags", Map.of("type", "text")),
-                  Map.entry("no_index_tags", Map.of("type", "text"))),
-              "id")
-          .wasSuccessful();
+  @BeforeAll
+  void createAndPopulateTableWithLexicalIndex() {
+    // Create a table with a lexical index on the "tags" column
+    assertNamespaceCommand(keyspaceName)
+        .templated()
+        .createTable(
+            TABLE_NAME,
+            Map.ofEntries(
+                Map.entry("id", Map.of("type", "text")),
+                Map.entry("value", Map.of("type", "text")),
+                Map.entry("tags", Map.of("type", "text")),
+                Map.entry("no_index_tags", Map.of("type", "text"))),
+            "id")
+        .wasSuccessful();
 
-      assertTableCommand(keyspaceName, TABLE_NAME)
-          .postCreateTextIndex(
-              """
+    assertTableCommand(keyspaceName, TABLE_NAME)
+        .postCreateTextIndex(
+            """
                                           {
                                             "name": "tags_lexical_idx",
                                             "definition": {
@@ -52,10 +50,10 @@ public class FindWithLexicalFilterTableIntegrationTest extends AbstractTableInte
                                             }
                                           }
                                           """)
-          .wasSuccessful();
-      assertTableCommand(keyspaceName, TABLE_NAME)
-          .postCreateIndex(
-              """
+        .wasSuccessful();
+    assertTableCommand(keyspaceName, TABLE_NAME)
+        .postCreateIndex(
+            """
                                 {
                                         "name": "value_idx",
                                         "definition": {
@@ -63,36 +61,34 @@ public class FindWithLexicalFilterTableIntegrationTest extends AbstractTableInte
                                         }
                                 }
                                 """)
-          .wasSuccessful();
+        .wasSuccessful();
 
-      // And then insert some data
-      insertOneInTable(
-          TABLE_NAME,
-          Map.of(
-              "id", "1",
-              "value", "a",
-              "tags", "tag1",
-              "no_index_tags", "abc"));
-      insertOneInTable(
-          TABLE_NAME, Map.of("id", "2", "value", "b", "tags", "tag2", "no_index_tags", "def"));
-      insertOneInTable(
-          TABLE_NAME,
-          Map.of(
-              "id", "3",
-              "value", "c",
-              "tags", "tag3 tag1",
-              "no_index_tags", "ghi"));
-      insertOneInTable(
-          TABLE_NAME,
-          Map.of(
-              "id", "4",
-              "value", "d",
-              "tags", "tag2 tag1 tag3",
-              "no_index_tags", "jkl"));
-    }
+    // And then insert some data
+    insertOneInTable(
+        TABLE_NAME,
+        Map.of(
+            "id", "1",
+            "value", "a",
+            "tags", "tag1",
+            "no_index_tags", "abc"));
+    insertOneInTable(
+        TABLE_NAME, Map.of("id", "2", "value", "b", "tags", "tag2", "no_index_tags", "def"));
+    insertOneInTable(
+        TABLE_NAME,
+        Map.of(
+            "id", "3",
+            "value", "c",
+            "tags", "tag3 tag1",
+            "no_index_tags", "ghi"));
+    insertOneInTable(
+        TABLE_NAME,
+        Map.of(
+            "id", "4",
+            "value", "d",
+            "tags", "tag2 tag1 tag3",
+            "no_index_tags", "jkl"));
   }
 
-  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
   @Nested
   @Order(5)
   class HappyLexicalFilter {
@@ -108,7 +104,6 @@ public class FindWithLexicalFilterTableIntegrationTest extends AbstractTableInte
   }
 
   // Failing cases
-  @DisabledIfSystemProperty(named = TEST_PROP_LEXICAL_DISABLED, matches = "true")
   @Nested
   @Order(10)
   class SadLexicalFilter {
