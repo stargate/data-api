496aa5f08fddc946425c955efd5a02223aa6a661
Hazel <hazel.he@datastax.com>
2025-09-05 14:42:36 -0700

    remove openai

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/InsertOneTableIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/tables/InsertOneTableIntegrationTest.java
@@ -1831,8 +1831,7 @@ public class InsertOneTableIntegrationTest extends AbstractTableIntegrationTestB
           .hasSingleApiError(
               ErrorCodeV1.EMBEDDING_PROVIDER_CLIENT_ERROR,
               anyOf(
-                  containsString(
-                      "Provider: openai; HTTP Status: 401; Error Message: \"Incorrect API key provided: test_emb"),
+                  containsString("Provider: openai; HTTP Status: 401; Error Message: "),
                   containsString("Provider: jinaAI; HTTP Status: 401; Error Message: ")));
     }
   }
