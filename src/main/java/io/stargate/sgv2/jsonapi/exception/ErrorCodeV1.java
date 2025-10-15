a01f4b508cab54fbb751291510c9f32e11a00443
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-10-15 16:52:43 -0700

    Suggestion from code review

--- src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
+++ src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
@@ -88,7 +88,7 @@ public enum ErrorCodeV1 {
   SHRED_DOC_LIMIT_VIOLATION("Document size limitation violated"),
 
   EXISTING_COLLECTION_DIFFERENT_SETTINGS("Collection already exists"),
-  EXISTING_TABLE_NOT_DATA_API_COLLECTION("Existing table not valid Data API collection"),
+  EXISTING_TABLE_NOT_DATA_API_COLLECTION("Existing table is not a valid Data API collection"),
   INVALID_VECTORIZE_VALUE_TYPE("$vectorize value needs to be text value"),
 
   INVALID_FILTER_EXPRESSION("Invalid filter expression"),
