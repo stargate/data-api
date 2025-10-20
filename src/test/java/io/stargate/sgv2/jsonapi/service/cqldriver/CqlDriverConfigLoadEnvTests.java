dfb361b82bc1f5f1d89bdd685409b89a7671fe91
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-10-20 11:48:48 -0700

    Extend test a bit to consider sysprop AND env vars

--- src/test/java/io/stargate/sgv2/jsonapi/service/cqldriver/CqlDriverConfigLoadEnvTests.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/cqldriver/CqlDriverConfigLoadEnvTests.java
@@ -1,16 +1,17 @@
 package io.stargate.sgv2.jsonapi.service.cqldriver;
 
+import static com.datastax.oss.driver.internal.core.config.typesafe.DefaultDriverConfigLoader.DEFAULT_ROOT_PATH;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
 import com.datastax.oss.driver.api.core.config.DriverConfig;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
+import com.datastax.oss.driver.api.core.config.DriverOption;
 import com.datastax.oss.driver.api.core.metadata.schema.SchemaChangeListener;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import io.stargate.sgv2.jsonapi.config.DatabaseType;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Supplier;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,8 +29,8 @@ import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 public class CqlDriverConfigLoadEnvTests {
   private static final String DEFAULT_SESSION = "tenant-none";
 
-  private static final String TENANT_OVERRIDE = "tenant-ENV";
-  private static final String CONSISTENCY_OVERRIDE = "ANY";
+  private static final String TENANT_ENV_OVERRIDE = "tenant-ENV";
+  private static final String CONSISTENCY_ENV_OVERRIDE = "ANY";
 
   @SystemStub private EnvironmentVariables environmentVariables;
 
@@ -55,7 +56,7 @@ public class CqlDriverConfigLoadEnvTests {
 
   @BeforeEach
   void beforeEach() {
-    // Test env-var for sanity checks
+    // Test env-var for sanity checks (to ensure tests set up envvars as expected)
     environmentVariables.set("env-debug", "true");
 
     // and then actual overrides
@@ -67,8 +68,8 @@ public class CqlDriverConfigLoadEnvTests {
     final String PREFIX_TYPESAFE = "CONFIG_FORCE_";
     final String FULL_PREFIX = PREFIX_TYPESAFE + "datastax__java__driver_";
 
-    environmentVariables.set(FULL_PREFIX + "basic_session__name", TENANT_OVERRIDE);
-    environmentVariables.set(FULL_PREFIX + "basic_request_consistency", CONSISTENCY_OVERRIDE);
+    environmentVariables.set(FULL_PREFIX + "basic_session__name", TENANT_ENV_OVERRIDE);
+    environmentVariables.set(FULL_PREFIX + "basic_request_consistency", CONSISTENCY_ENV_OVERRIDE);
   }
 
   // Sanity check for Env var overridability (outside of config loading)
@@ -92,18 +93,56 @@ public class CqlDriverConfigLoadEnvTests {
         DriverConfigLoader.programmaticBuilder()
             .withString(DefaultDriverOption.SESSION_NAME, DEFAULT_SESSION)
             .build();
-    verifyTenantAndConsistency(loader.getInitialConfig(), TENANT_OVERRIDE, CONSISTENCY_OVERRIDE);
+    verifyTenantAndConsistency(
+        loader.getInitialConfig(), TENANT_ENV_OVERRIDE, CONSISTENCY_ENV_OVERRIDE);
+  }
+
+  // Tests for override via ENV variables AND sysprops over defaults -- env vars
+  // documented to have precedence
+  @Test
+  void testEnvVarAndSysPropsOverrides() {
+    final String TENANT_SYSPROP_OVERRIDE = "tenant-sysprop";
+    final String CONSISTENCY_SYSPROP_OVERRIDE = "QUORUM";
+    try {
+      setSystemProperty(DefaultDriverOption.SESSION_NAME, TENANT_SYSPROP_OVERRIDE);
+      setSystemProperty(DefaultDriverOption.REQUEST_CONSISTENCY, CONSISTENCY_SYSPROP_OVERRIDE);
+
+      DriverConfigLoader loader =
+          DriverConfigLoader.programmaticBuilder()
+              .withString(DefaultDriverOption.SESSION_NAME, DEFAULT_SESSION)
+              .build();
+      // ENV vars should override everything else
+      verifyTenantAndConsistency(
+          loader.getInitialConfig(), TENANT_ENV_OVERRIDE, CONSISTENCY_ENV_OVERRIDE);
+    } finally {
+      clearSystemProperty(DefaultDriverOption.SESSION_NAME);
+      clearSystemProperty(DefaultDriverOption.REQUEST_CONSISTENCY);
+    }
   }
 
   private void verifyTenantAndConsistency(
       DriverConfig config, String expTenant, String expConsistency) {
+    /*
     System.err.println("CONFIG");
     for (Map.Entry<String, Object> entry : config.getDefaultProfile().entrySet()) {
       System.err.println(" '" + entry.getKey() + "' -> " + entry.getValue());
     }
+       */
     assertThat(config.getDefaultProfile().getString(DefaultDriverOption.REQUEST_CONSISTENCY))
         .isEqualTo(expConsistency);
     assertThat(config.getDefaultProfile().getString(DefaultDriverOption.SESSION_NAME))
         .isEqualTo(expTenant);
+  }
+
+  private static void clearSystemProperty(DriverOption option) {
+    System.clearProperty(path(option));
+  }
+
+  private static void setSystemProperty(DriverOption option, String value) {
+    System.setProperty(path(option), value);
+  }
+
+  private static String path(DriverOption option) {
+    return DEFAULT_ROOT_PATH + "." + option.getPath();
   }
 }
