222e1a919f3efcf5be89bb262ee6c91e35d7899f
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-10-28 08:53:08 -0700

    Fixes #2220, update to DSE 6.9.15 (from 6.9.14) (#2237)

--- src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
+++ src/test/java/io/stargate/sgv2/jsonapi/testresource/DseTestResource.java
@@ -44,7 +44,7 @@ public class DseTestResource extends StargateTestResource {
     // 21-Apr-2025, tatu: formerly referenced hard-coded images; left here for reference:
     //   to be removed in near future
     // "stargateio/dse-next:4.0.11-591d171ac9c9"
-    // "datastax/dse-server:6.9.14"
+    // "datastax/dse-server:6.9.15"
     // "559669398656.dkr.ecr.us-west-2.amazonaws.com/engops-shared/hcd/prod/hcd:1.2.3";
 
     // 21-Apr-2025, tatu: [data-api#1952] Load definition from "./docker-compose/.env"
