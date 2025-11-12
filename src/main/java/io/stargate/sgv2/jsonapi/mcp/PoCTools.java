4612401a903105debbd12e2ecf3442202d398d5a
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-11-11 16:58:51 -0800

    Add "addTool" tool

--- src/main/java/io/stargate/sgv2/jsonapi/mcp/PoCTools.java
+++ src/main/java/io/stargate/sgv2/jsonapi/mcp/PoCTools.java
@@ -3,6 +3,8 @@ package io.stargate.sgv2.jsonapi.mcp;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.quarkiverse.mcp.server.Tool;
 import io.quarkiverse.mcp.server.ToolArg;
+import io.quarkiverse.mcp.server.ToolManager;
+import io.quarkiverse.mcp.server.ToolResponse;
 import io.stargate.sgv2.jsonapi.ConfigPreLoader;
 import io.stargate.sgv2.jsonapi.api.model.command.CommandContext;
 import io.stargate.sgv2.jsonapi.api.request.RequestContext;
@@ -14,6 +16,8 @@ import jakarta.inject.Inject;
 import java.util.Map;
 
 public class PoCTools {
+  @Inject private ToolManager toolManager;
+
   @Inject private RequestContext requestContext;
   @Inject private SchemaCache schemaCache;
 
@@ -37,11 +41,12 @@ public class PoCTools {
   }
 
   @Tool(description = "Simple Echo tool")
-  String echo(@ToolArg(description = "Response", defaultValue = "OK") String response) {
+  String echo(
+      @ToolArg(description = "Response", defaultValue = "OK", required = false) String response) {
     return response;
   }
 
-  @Tool(description = "System info printer")
+  @Tool(description = "System info printer tool")
   Map<String, String> sysinfo() {
     return Map.of(
         "requestId",
@@ -50,5 +55,19 @@ public class PoCTools {
         requestContext.getTenantId().orElse("N/A"),
         "token",
         requestContext.getCassandraToken().orElse("N/A"));
+  }
+
+  @Tool(description = "Add Tool tool")
+  String addTool(@ToolArg(description = "Name") String name) {
+    if (toolManager.getTool(name) != null) {
+      return "ALREADY_EXISTS";
+    }
+    toolManager
+        .newTool(name)
+        .setDescription("Tool '" + name + "': lower-cases given String")
+        .addArgument("value", "Value to convert", true, String.class)
+        .setHandler(ta -> ToolResponse.success(ta.args().get("value").toString().toLowerCase()))
+        .register();
+    return "ADDED";
   }
 }
