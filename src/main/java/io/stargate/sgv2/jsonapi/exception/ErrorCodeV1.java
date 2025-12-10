2c617274e89300b6b648b5b2c92e0485775c0187
Tatu Saloranta <tatu.saloranta@datastax.com>
2025-12-09 17:24:57 -0800

    Continue #2285: remove some more ErrorCodeV1 entries

--- src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
+++ src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
@@ -160,7 +160,6 @@ public enum ErrorCodeV1 {
   VECTORIZE_CREDENTIAL_INVALID("Invalid credential name for vectorize"),
   VECTORIZECONFIG_CHECK_FAIL("Internal server error: VectorizeDefinition check fail"),
 
-  LEXICAL_NOT_AVAILABLE_FOR_DATABASE("Lexical search is not available on this database"),
   LEXICAL_CONTENT_TOO_BIG(
       "Lexical content is too big, please use a smaller value for the $lexical field"),
 
