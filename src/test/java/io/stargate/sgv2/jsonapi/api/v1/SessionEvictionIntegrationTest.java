8f4a7c472f702c4743f3695cd2e2b3d28f01b360
Hazel <hazel.he@datastax.com>
2026-01-05 16:47:45 -0800

    test

--- src/test/java/io/stargate/sgv2/jsonapi/api/v1/SessionEvictionIntegrationTest.java
+++ src/test/java/io/stargate/sgv2/jsonapi/api/v1/SessionEvictionIntegrationTest.java
@@ -1,7 +1,10 @@
 package io.stargate.sgv2.jsonapi.api.v1;
 
 import static io.stargate.sgv2.jsonapi.api.v1.ResponseAssertions.*;
+import static org.hamcrest.Matchers.containsString;
 
+import com.github.dockerjava.api.DockerClient;
+import io.quarkus.logging.Log;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.stargate.sgv2.jsonapi.testresource.DseTestResource;
@@ -14,7 +17,7 @@ import org.testcontainers.containers.GenericContainer;
 public class SessionEvictionIntegrationTest extends AbstractCollectionIntegrationTestBase {
 
   @Test
-  public void testSessionEvictionOnAllNodesFailed() throws InterruptedException {
+  public void testSessionEvictionOnAllNodesFailed() throws Exception {
 
     insertDoc(
         """
@@ -28,12 +31,10 @@ public class SessionEvictionIntegrationTest extends AbstractCollectionIntegratio
               """);
 
     GenericContainer<?> dbContainer = StargateTestResource.getCassandraContainer();
-    if (dbContainer == null) {
-      throw new RuntimeException("Cannot find Cassandra container.");
-    }
-
-    System.out.println("Stopping Database Container to simulate failure...");
-    dbContainer.stop();
+    DockerClient dockerClient = dbContainer.getDockerClient();
+    String containerId = dbContainer.getContainerId();
+    Log.info("Pausing Database Container to simulate failure (Freeze)...");
+    dockerClient.pauseContainerCmd(containerId).exec();
 
     // Should get AllNodeFailedException
     givenHeadersPostJsonThen(
@@ -47,18 +48,21 @@ public class SessionEvictionIntegrationTest extends AbstractCollectionIntegratio
               }
               """)
         .statusCode(500)
-        .body("$", responseIsError());
+        .body("$", responseIsErrorWithStatus())
+        .body("errors[0].message", containsString("No node was available"));
 
     // restart container
-    System.out.println("Starting Database Container to simulate recovery...");
-    dbContainer.start();
+    Log.info("Unpausing Database Container to simulate recovery...");
+    dockerClient.unpauseContainerCmd(containerId).exec();
 
     // wait
-    Thread.sleep(20000);
+    Log.info("start to sleep");
+    Thread.sleep(30000);
+    Log.info("end sleep");
 
     // restore
-    createKeyspace();
-    createDefaultCollection();
+    //    createKeyspace();
+    //    createDefaultCollection();
 
     // verify
     insertDoc(
@@ -66,12 +70,12 @@ public class SessionEvictionIntegrationTest extends AbstractCollectionIntegratio
               {
                 "insertOne": {
                   "document": {
-                    "name": "after_crash"
+                    "name": "after_recovery"
                   }
                 }
               }
               """);
 
-    System.out.println("Test Passed: Session recovered after DB restart.");
+    Log.info("Test Passed: Session recovered after DB restart.");
   }
 }
