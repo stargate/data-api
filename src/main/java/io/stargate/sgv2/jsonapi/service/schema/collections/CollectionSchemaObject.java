67cf273787ad8d3c0aea8a8039569383d0f893a8
Tatu Saloranta <tatu.saloranta@datastax.com>
2026-01-07 11:54:53 -0800

    Convert second code

--- src/main/java/io/stargate/sgv2/jsonapi/service/schema/collections/CollectionSchemaObject.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/schema/collections/CollectionSchemaObject.java
@@ -16,7 +16,7 @@ import io.stargate.sgv2.jsonapi.api.model.command.impl.VectorizeConfig;
 import io.stargate.sgv2.jsonapi.config.constants.DocumentConstants;
 import io.stargate.sgv2.jsonapi.config.constants.TableCommentConstants;
 import io.stargate.sgv2.jsonapi.config.constants.VectorConstants;
-import io.stargate.sgv2.jsonapi.exception.ErrorCodeV1;
+import io.stargate.sgv2.jsonapi.exception.DatabaseException;
 import io.stargate.sgv2.jsonapi.exception.ServerException;
 import io.stargate.sgv2.jsonapi.service.cqldriver.executor.*;
 import io.stargate.sgv2.jsonapi.service.projection.IndexingProjector;
@@ -335,15 +335,22 @@ public final class CollectionSchemaObject extends TableBasedSchemaObject {
         final JsonNode schemaVersionNode =
             collectionNode.get(TableCommentConstants.SCHEMA_VERSION_KEY);
         if (schemaVersionNode == null) {
-          throw ErrorCodeV1.INVALID_SCHEMA_VERSION.toApiException();
+          throw DatabaseException.Code.COLLECTION_SCHEMA_VERSION_INVALID.get(
+              Map.of("collectionName", collectionName, "schemaVersion", "<null>"));
         }
-        switch (collectionNode.get(TableCommentConstants.SCHEMA_VERSION_KEY).asInt()) {
+        int schemaVersion = collectionNode.get(TableCommentConstants.SCHEMA_VERSION_KEY).asInt();
+        switch (schemaVersion) {
           case 1:
             return new CollectionSettingsV1Reader()
                 .readCollectionSettings(
                     collectionNode, keyspaceName, collectionName, tableMetadata, objectMapper);
           default:
-            throw ErrorCodeV1.INVALID_SCHEMA_VERSION.toApiException();
+            throw DatabaseException.Code.COLLECTION_SCHEMA_VERSION_INVALID.get(
+                Map.of(
+                    "collectionName",
+                    collectionName,
+                    "schemaVersion",
+                    String.valueOf(schemaVersion)));
         }
       } else {
         // backward compatibility for old indexing table comment
