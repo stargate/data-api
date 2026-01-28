e46aa5a6c656f5043fa90676a5552bee5696b8db
Hazel <hazel.he@datastax.com>
2026-01-28 13:25:08 -0800

    convert EMBEDDING_PROVIDER_TIMEOUT

--- src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
+++ src/main/java/io/stargate/sgv2/jsonapi/exception/ErrorCodeV1.java
@@ -6,7 +6,6 @@ import jakarta.ws.rs.core.Response;
 public enum ErrorCodeV1 {
   /** Embedding provider service error codes. */
   // !!! 16-Dec-2025, tatu: USED BY EMBEDDING-GATEWAY, DO NOT CONVERT "EMBEDDING_" entries yet
-  EMBEDDING_PROVIDER_TIMEOUT("The Embedding Provider timed out"),
   EMBEDDING_PROVIDER_UNEXPECTED_RESPONSE("The Embedding Provider returned an unexpected response"),
 
   UNSUPPORTED_UPDATE_DATA_TYPE("Unsupported update data type"),
