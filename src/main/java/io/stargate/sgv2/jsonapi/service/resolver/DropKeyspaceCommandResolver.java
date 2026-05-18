f034b3b607fabd2984a3af6d5191bcb0a616f310
Eric Hare <ericrhare@gmail.com>
2026-05-18 09:55:42 -0700

    security: validate keyspace and datacenter names to prevent CQL injection (GHSA-p64p-96pw-mxwv) (#2472)

--- src/main/java/io/stargate/sgv2/jsonapi/service/resolver/DropKeyspaceCommandResolver.java
+++ src/main/java/io/stargate/sgv2/jsonapi/service/resolver/DropKeyspaceCommandResolver.java
@@ -5,6 +5,7 @@ import io.stargate.sgv2.jsonapi.api.model.command.impl.DropKeyspaceCommand;
 import io.stargate.sgv2.jsonapi.service.operation.Operation;
 import io.stargate.sgv2.jsonapi.service.operation.keyspaces.DropKeyspaceOperation;
 import io.stargate.sgv2.jsonapi.service.schema.DatabaseSchemaObject;
+import io.stargate.sgv2.jsonapi.service.schema.naming.NamingRules;
 import jakarta.enterprise.context.ApplicationScoped;
 
 /** Command resolver for {@link DropKeyspaceCommand}. */
@@ -21,6 +22,7 @@ public class DropKeyspaceCommandResolver implements CommandResolver<DropKeyspace
   @Override
   public Operation resolveDatabaseCommand(
       CommandContext<DatabaseSchemaObject> ctx, DropKeyspaceCommand command) {
-    return new DropKeyspaceOperation(command.name());
+    var keyspaceName = NamingRules.KEYSPACE.checkRule(command.name());
+    return new DropKeyspaceOperation(keyspaceName);
   }
 }
