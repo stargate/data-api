0c9a4b34f84488865b687c78918260ad273f14f1
Hazel <hazel.he@datastax.com>
2026-08-04 18:55:15 -0700

    format

--- src/main/java/io/stargate/sgv2/jsonapi/config/DeploymentMarkerConfig.java
+++ src/main/java/io/stargate/sgv2/jsonapi/config/DeploymentMarkerConfig.java
@@ -4,10 +4,8 @@ import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import java.util.Optional;
 
-
 @ConfigMapping(prefix = "stargate.jsonapi.deployment-marker")
 public interface DeploymentMarkerConfig {
-
 
   @WithDefault("false")
   boolean enabled();
