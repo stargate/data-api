376de9fae2031f2419a48f341e96106a0b0375e2
Aaron Morton <aaron.morton@datastax.com>
2026-08-13 12:08:16 +1200

    FIX: #2539 check CqlSession has metadata (#2542)

--- src/test/java/io/stargate/sgv2/jsonapi/service/cqldriver/CqlSessionFactoryTests.java
+++ src/test/java/io/stargate/sgv2/jsonapi/service/cqldriver/CqlSessionFactoryTests.java
@@ -1,19 +1,33 @@
 package io.stargate.sgv2.jsonapi.service.cqldriver;
 
+import static io.stargate.sgv2.jsonapi.service.cqldriver.executor.DefaultDriverExceptionHandlerTest.mockNode;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
+import com.datastax.oss.driver.api.core.AllNodesFailedException;
+import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+import com.datastax.oss.driver.api.core.auth.AuthenticationException;
 import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
+import com.datastax.oss.driver.api.core.metadata.Metadata;
+import com.datastax.oss.driver.api.core.metadata.schema.KeyspaceMetadata;
 import com.datastax.oss.driver.api.core.metadata.schema.SchemaChangeListener;
+import com.datastax.oss.driver.internal.core.metadata.DefaultEndPoint;
 import io.stargate.sgv2.jsonapi.TestConstants;
 import io.stargate.sgv2.jsonapi.api.request.tenant.Tenant;
+import io.stargate.sgv2.jsonapi.exception.APISecurityException;
+import io.stargate.sgv2.jsonapi.exception.DatabaseException;
+import io.stargate.sgv2.jsonapi.exception.ExceptionFlags;
 import io.stargate.sgv2.jsonapi.service.cqldriver.executor.optvector.SubtypeOnlyFloatVectorToArrayCodec;
 import java.net.InetSocketAddress;
+import java.util.AbstractMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
@@ -33,6 +47,99 @@ public class CqlSessionFactoryTests {
     var fixture = newFixture(TEST_CONSTANTS.TENANT, endpoints, schemaListener);
 
     assertions(fixture, endpoints, schemaListener);
+  }
+
+  @Test
+  public void createAstraDbSessionAuthenticationError() {
+
+    var schemaListener = mock(SchemaChangeListener.class);
+    var endpoints = List.<String>of();
+
+    var localHost = InetSocketAddress.createUnresolved("localhost", 9042);
+    var authErr =
+        new AuthenticationException(
+            new DefaultEndPoint(localHost), "A FAKE authentication error occurred");
+
+    var node = mockNode("node: " + localHost);
+    var allNodesErr =
+        AllNodesFailedException.fromErrors(List.of(new AbstractMap.SimpleEntry<>(node, authErr)));
+
+    var fixture =
+        newFixture(TEST_CONSTANTS.TENANT, endpoints, schemaListener, allNodesErr, true, null);
+
+    // because the CqlSessionFactory is working through java CompletionStage the exception
+    // the cause is smuggled out in CompletionException
+    assertThatThrownBy(() -> assertions(fixture, endpoints, schemaListener))
+        .as("Authentication error from driver mapped")
+        .isInstanceOfSatisfying(
+            CompletionException.class,
+            completionException -> {
+              assertThat(completionException.getCause()).isInstanceOf(APISecurityException.class);
+              APISecurityException err = (APISecurityException) completionException.getCause();
+              assertThat(err.code)
+                  .isEqualTo(APISecurityException.Code.UNAUTHENTICATED_REQUEST.name());
+              // this is one of the few situations where we return non HTTP 200
+              assertThat(err.httpStatus).as("Authentication error is HTTP 401").isEqualTo(401);
+            });
+  }
+
+  @Test
+  public void createAstraDbSessionMissingMetadata() {
+
+    var schemaListener = mock(SchemaChangeListener.class);
+    var endpoints = List.<String>of();
+
+    var fixture = newFixture(TEST_CONSTANTS.TENANT, endpoints, schemaListener, null, false, null);
+
+    // because the CqlSessionFactory is working through java CompletionStage the exception
+    // the cause is smuggled out in CompletionException
+    assertThatThrownBy(() -> assertions(fixture, endpoints, schemaListener))
+        .as("Authentication error from driver mapped")
+        .isInstanceOfSatisfying(
+            CompletionException.class,
+            completionException -> {
+              assertThat(completionException.getCause()).isInstanceOf(DatabaseException.class);
+              DatabaseException err = (DatabaseException) completionException.getCause();
+              assertThat(err.code).isEqualTo(DatabaseException.Code.FAILED_TO_READ_METADATA.name());
+              // this is one of the few situations where we return non HTTP 200
+              assertThat(err.exceptionFlags)
+                  .as("Exception flagged as UNRELIABLE_DB_SESSION")
+                  .contains(ExceptionFlags.UNRELIABLE_DB_SESSION);
+            });
+
+    // confirming we are correctly closing the session if metadata is missing
+    verify(fixture.session()).closeAsync();
+  }
+
+  @Test
+  public void createAstraDbSessionMissingMetadataErrorClosing() {
+
+    var schemaListener = mock(SchemaChangeListener.class);
+    var endpoints = List.<String>of();
+
+    var closingError = new RuntimeException("FAKE closeAsync() exception");
+    var fixture =
+        newFixture(TEST_CONSTANTS.TENANT, endpoints, schemaListener, null, false, closingError);
+
+    // same asserts as createAstraDbSessionMissingMetadata - but this is checking that even if
+    // closeAsync() throws
+    // the returned error to the user is FAILED_TO_READ_METADATA
+    assertThatThrownBy(() -> assertions(fixture, endpoints, schemaListener))
+        .as("Authentication error from driver mapped")
+        .isInstanceOfSatisfying(
+            CompletionException.class,
+            completionException -> {
+              assertThat(completionException.getCause()).isInstanceOf(DatabaseException.class);
+              DatabaseException err = (DatabaseException) completionException.getCause();
+              assertThat(err.code).isEqualTo(DatabaseException.Code.FAILED_TO_READ_METADATA.name());
+              // this is one of the few situations where we return non HTTP 200
+              assertThat(err.exceptionFlags)
+                  .as("Exception flagged as UNRELIABLE_DB_SESSION")
+                  .contains(ExceptionFlags.UNRELIABLE_DB_SESSION);
+            });
+
+    // confirming we are correctly closing the session if metadata is missing
+    verify(fixture.session()).closeAsync();
   }
 
   @Test
@@ -104,10 +211,33 @@ public class CqlSessionFactoryTests {
 
   private Fixture newFixture(
       Tenant tenant, List<String> endpoints, SchemaChangeListener schemaChangeListener) {
+    return newFixture(tenant, endpoints, schemaChangeListener, null, true, null);
+  }
+
+  private Fixture newFixture(
+      Tenant tenant,
+      List<String> endpoints,
+      SchemaChangeListener schemaChangeListener,
+      RuntimeException error,
+      boolean withMetadata,
+      RuntimeException closingError) {
 
     // we are testing that the CqlSessionFactory calls the session builder correctly,
     // so we mock the session builder and verify that it is called correctly.
     var session = mock(CqlSession.class);
+
+    // CqlSession guarantees a Metdata obj, and we now check it
+    var metadata = mock(Metadata.class);
+    when(session.getMetadata()).thenReturn(metadata);
+    if (closingError == null) {
+      when(session.closeAsync()).thenReturn(CompletableFuture.completedFuture(null));
+    } else {
+      when(session.closeAsync()).thenReturn(CompletableFuture.failedFuture(closingError));
+    }
+    Optional<KeyspaceMetadata> keyspaceMetadata =
+        withMetadata ? Optional.of(mock(KeyspaceMetadata.class)) : Optional.empty();
+    when(metadata.getKeyspace(any(CqlIdentifier.class))).thenReturn(keyspaceMetadata);
+    when(metadata.getKeyspace(anyString())).thenReturn(keyspaceMetadata);
 
     var sessionBuilder = mock(CqlSessionBuilder.class);
     when(sessionBuilder.withLocalDatacenter(any())).thenReturn(sessionBuilder);
@@ -119,8 +249,15 @@ public class CqlSessionFactoryTests {
     when(sessionBuilder.addContactPoints(any())).thenReturn(sessionBuilder);
     when(sessionBuilder.addTypeCodecs(any())).thenReturn(sessionBuilder);
 
-    when(sessionBuilder.buildAsync()).thenReturn(CompletableFuture.completedFuture(session));
-
+    if (error != null) {
+      // when the driver completes any error is generates is wrapped in
+      // CompletionException() because the thown error has passed through stages.
+      // wrapping here to make it the same
+      when(sessionBuilder.buildAsync())
+          .thenReturn(CompletableFuture.failedFuture(new CompletionException(error)));
+    } else {
+      when(sessionBuilder.buildAsync()).thenReturn(CompletableFuture.completedFuture(session));
+    }
     var credentials = mock(CqlCredentials.class);
     when(credentials.addToSessionBuilder(any())).thenReturn(sessionBuilder);
 
